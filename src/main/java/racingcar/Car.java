c401e78347ff06f6cb1845e1f6cbe87bb7411304
plan11plan <tjtfy7028@gmail.com>
2024-11-15 21:11:24 +0900

    feat(Car): 자동차 객체 생성
    - 이름 글자 제한 : 1~5자 까지 가능 하다.

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+public class Car {
+    private final String name;
+    private final int moveAmount;
+
+    public Car(String name, int moveAmount) {
+        this.name = name;
+        this.moveAmount = moveAmount;
+    }
+    private void validateName(){
+        if(name.length() < 1 || name.length() > 5){
+            throw new IllegalArgumentException("[ERROR] 자동차 이름의 길이를 0~5글자 사이로 입력 해 주세요.");
+        }
+    }
+
+}
