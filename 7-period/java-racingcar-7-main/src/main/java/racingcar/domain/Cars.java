7cfd79ab67c970a661fb5874dad29cf897b6c4f1
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-15 22:23:29 +0900

    feat(Cars): 자동차들을 관리하는 도메인 구현

---
+++ 7-period/java-racingcar-7-main/src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,35 @@
+package racingcar.domain;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Cars {
+
+    private static final int ZERO = 0;
+    private final List<Car> cars;
+
+    public Cars(final List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public List<Car> move() {
+        for (Car car : cars) {
+            car.move();
+        }
+        return Collections.unmodifiableList(this.cars);
+    }
+
+    public List<Car> getWinners() {
+        final int score = calculateMaxScore();
+        return cars.stream()
+                .filter(car -> car.isSameScore(score))
+                .toList();
+    }
+
+    private int calculateMaxScore() {
+        return cars.stream()
+                .mapToInt(Car::getScore)
+                .max()
+                .orElse(ZERO);
+    }
+}
