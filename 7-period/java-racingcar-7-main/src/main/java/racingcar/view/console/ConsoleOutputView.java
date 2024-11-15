d9a60110450085e0bf6deb9a172861ab097ddfa6
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-15 22:53:05 +0900

    feat(ConsoleOutputView): 내부 메서드들 구현

--- 7-period/java-racingcar-7-main/src/main/java/racingcar/view/console/ConsoleOutputView.java
+++ 7-period/java-racingcar-7-main/src/main/java/racingcar/view/console/ConsoleOutputView.java
@@ -1,4 +1,49 @@
 package racingcar.view.console;
 
-public class ConsoleOutputView {
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.dto.RacingProgress;
+import racingcar.view.OutputView;
+
+public class ConsoleOutputView implements OutputView {
+
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+    private static final String ASK_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ASK_ATTEMPT = "시도할 횟수는 몇 회인가요?";
+    private static final String PROGRESSES_TITLE = "실행 결과";
+    private static final String HYPHEN = "-";
+    private static final String PROGRESS = "%s : %s";
+    private static final String WINNERS = "최종 우승자 : %s";
+    private static final String DELIMITER_COMMA = ", ";
+
+
+    @Override
+    public void printAskCarNames() {
+        printlnMessage(ASK_CAR_NAMES);
+    }
+
+    @Override
+    public void printAskAttempt() {
+        printlnMessage(ASK_ATTEMPT);
+    }
+
+    @Override
+    public void printRacingProgresses(final List<RacingProgress> racingProgresses) {
+        final String message = racingProgresses.stream()
+                .map(racingProgress -> String.format(PROGRESS, racingProgress.carName(),
+                        HYPHEN.repeat(racingProgress.score())))
+                .collect(Collectors.joining(LINE_SEPARATOR));
+
+        printlnMessage(PROGRESSES_TITLE + LINE_SEPARATOR + message);
+    }
+
+    @Override
+    public void printRacingWinners(final List<String> racingWinners) {
+        final String message = String.format(WINNERS, String.join(DELIMITER_COMMA, racingWinners));
+        printlnMessage(message);
+    }
+
+    private void printlnMessage(final String message) {
+        System.out.println(message);
+    }
 }
