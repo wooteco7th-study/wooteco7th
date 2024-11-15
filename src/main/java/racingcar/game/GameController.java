f1e6df17d2b09304f6bc8b4d44abbb56a0e2baaa
plan11plan <tjtfy7028@gmail.com>
2024-11-15 23:52:10 +0900

    refactor: 큰 메서드 작은 메서드들로 분리

--- src/main/java/racingcar/game/GameController.java
+++ src/main/java/racingcar/game/GameController.java
@@ -1,6 +1,7 @@
 package racingcar.game;
 
 import java.util.List;
+import java.util.stream.IntStream;
 import racingcar.car.Car;
 import racingcar.car.GameResult;
 import racingcar.car.RaceInfo;
@@ -19,19 +20,33 @@ public class GameController {
         this.gameService = gameService;
     }
 
+
     public void run() {
+        List<Car> cars = initializeGame();
+        int rounds = getRoundCount();
+        showGameProgress(cars, rounds);
+        announceResult(cars, rounds);
+    }
+
+    private List<Car> initializeGame() {
         List<String> carNames = inputView.requestCarNames();
-        List<Car> cars = carService.createCarsByNames(carNames);
-        int rounds = inputView.requestRoundCount();
+        return carService.createCarsByNames(carNames);
+    }
 
+    private int getRoundCount() {
+        return inputView.requestRoundCount();
+    }
+
+    private void showGameProgress(List<Car> cars, int rounds) {
         inputView.messageInitiateGameResult();
+        IntStream.range(0, rounds)
+                .forEach(i -> {
+                    List<RaceInfo> raceInfos = gameService.play(cars, rounds);
+                    outputView.printRaceInfos(raceInfos);
+                });
+    }
 
-        List<Car> currentRoundCars = cars;
-        for (int i = 0; i < rounds; i++) {
-            List<RaceInfo> raceInfos = gameService.play(cars, rounds);
-            outputView.printRaceInfos(raceInfos);
-        }
-
+    private void announceResult(List<Car> cars, int rounds) {
         GameResult gameResult = new GameResult(gameService.play(cars, rounds));
         outputView.announceWinners(gameResult.pickWinners());
     }
