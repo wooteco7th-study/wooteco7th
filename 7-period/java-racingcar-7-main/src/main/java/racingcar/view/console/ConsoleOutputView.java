5f476c9ef9821cfa1bcd5c2b60e4a8bfffee3645
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-15 23:12:53 +0900

    refactor(ConsoleOutputView): 실행 결과 출력 메서드 수정

--- 7-period/java-racingcar-7-main/src/main/java/racingcar/view/console/ConsoleOutputView.java
+++ 7-period/java-racingcar-7-main/src/main/java/racingcar/view/console/ConsoleOutputView.java
@@ -3,6 +3,7 @@ package racingcar.view.console;
 import java.util.List;
 import java.util.stream.Collectors;
 import racingcar.dto.RaceProgress;
+import racingcar.dto.RaceResult;
 import racingcar.view.OutputView;
 
 public class ConsoleOutputView implements OutputView {
@@ -10,7 +11,7 @@ public class ConsoleOutputView implements OutputView {
     private static final String LINE_SEPARATOR = System.lineSeparator();
     private static final String ASK_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ASK_ATTEMPT = "시도할 횟수는 몇 회인가요?";
-    private static final String PROGRESSES_TITLE = "실행 결과";
+    private static final String RACE_RESULT_TITLE = "실행 결과";
     private static final String HYPHEN = "-";
     private static final String PROGRESS = "%s : %s";
     private static final String WINNERS = "최종 우승자 : %s";
@@ -28,19 +29,25 @@ public class ConsoleOutputView implements OutputView {
     }
 
     @Override
-    public void printRacingProgresses(final List<RaceProgress> raceProgresses) {
-        final String message = raceProgresses.stream()
-                .map(raceProgress -> String.format(PROGRESS, raceProgress.carName(),
-                        HYPHEN.repeat(raceProgress.score())))
+    public void printRaceResults(final List<RaceResult> raceResults) {
+        final String message = raceResults.stream()
+                .map(this::generateRaceResultMessage)
                 .collect(Collectors.joining(LINE_SEPARATOR));
-
-        printlnMessage(PROGRESSES_TITLE + LINE_SEPARATOR + message);
+        printlnMessage(RACE_RESULT_TITLE + LINE_SEPARATOR + message);
     }
 
     @Override
-    public void printRacingWinners(final List<String> racingWinners) {
+    public void printRaceWinners(final List<String> racingWinners) {
         final String message = String.format(WINNERS, String.join(DELIMITER_COMMA, racingWinners));
         printlnMessage(message);
+    }
+
+    private String generateRaceResultMessage(final RaceResult raceResult) {
+        final List<RaceProgress> raceProgresses = raceResult.raceProgresses();
+        return raceProgresses.stream()
+                .map(raceProgress -> String.format(PROGRESS, raceProgress.carName(),
+                        HYPHEN.repeat(raceProgress.score())))
+                .collect(Collectors.joining(LINE_SEPARATOR));
     }
 
     private void printlnMessage(final String message) {
