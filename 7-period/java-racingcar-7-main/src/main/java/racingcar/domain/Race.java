696b1d8e6fb10dda1693f2e9ce9c26500499b62a
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-15 23:08:02 +0900

    refactor(Race): 클래스 이름 수정

--- 7-period/java-racingcar-7-main/src/main/java/racingcar/domain/Race.java
+++ 7-period/java-racingcar-7-main/src/main/java/racingcar/domain/Race.java
@@ -3,12 +3,12 @@ package racingcar.domain;
 import java.util.Collections;
 import java.util.List;
 
-public class Cars {
+public class Race {
 
     private static final int ZERO = 0;
     private final List<Car> cars;
 
-    public Cars(final List<Car> cars) {
+    public Race(final List<Car> cars) {
         this.cars = cars;
     }
 
