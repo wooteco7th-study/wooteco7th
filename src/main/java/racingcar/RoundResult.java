58ab022ad25c4e613b7997c3ad25d2408216c0d9
plan11plan <tjtfy7028@gmail.com>
2024-11-15 21:53:10 +0900

    feat(RoundResult): 최종 우승자를 뽑는 기능 구현

--- src/main/java/racingcar/RoundResult.java
+++ src/main/java/racingcar/RoundResult.java
@@ -10,4 +10,17 @@ public class RoundResult {
         this.raceInfos = raceInfos;
     }
 
+    public Winners pickWinners() {
+        int maxMove = raceInfos.stream()
+                .mapToInt(info -> info.getTotalMoveAmount())
+                .max()
+                .orElse(0);
+
+        List<String> winners = raceInfos.stream()
+                .filter(info -> info.getTotalMoveAmount() == maxMove)
+                .map(i->i.getName())
+                .toList();
+        return new Winners(winners);
+    }
+
 }
---
+++ src/main/java/racingcar/Winners.java
@@ -0,0 +1,8 @@
+package racingcar;
+
+import java.util.List;
+
+public record Winners(
+        List<String> winners
+){
+}
