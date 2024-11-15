1621a5ab65544774596c181f36b433ab01a1e74e
plan11plan <tjtfy7028@gmail.com>
2024-11-15 21:44:30 +0900

    refactor(RaceInfo): 불변 객체로 리팩토링

--- src/main/java/racingcar/RaceInfo.java
+++ src/main/java/racingcar/RaceInfo.java
@@ -2,11 +2,19 @@ package racingcar;
 
 // record 일부러 안씀. record 쓰면 해쉬코드,equals 자동 생성이기 때문에, 중복 이름 허용 무시하게됨.
 public class RaceInfo {
-    String name;
-    int totalMoveAmount;
+    private final String name;
+    private final int totalMoveAmount;
 
     public RaceInfo(Car car) {
         this.name = car.getName();
         this.totalMoveAmount = car.getMoveAmount();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTotalMoveAmount() {
+        return totalMoveAmount;
     }
 }
