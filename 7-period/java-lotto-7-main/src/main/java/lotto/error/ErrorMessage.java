d8fec5ea831a3d860aa60df5f0c69d3914950b4f
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-17 19:14:56 +0900

    feat(ErrorMessage, WrongLottoNumberSizeException): 로또 숫자 사이즈 예외 추가

--- 7-period/java-lotto-7-main/src/main/java/lotto/error/ErrorMessage.java
+++ 7-period/java-lotto-7-main/src/main/java/lotto/error/ErrorMessage.java
@@ -6,7 +6,9 @@ public enum ErrorMessage {
     INVALID_DUPLICATED_LOTTO_NUMBER("로또 번호는 중복 될 수 없습니다."),
     INVALID_WRONG_NUMBER_FORMAT("구입 금액과 번호는 숫자여야 합니다."),
     INVALID_WRONG_MONEY_UNIT("구입 금액은 1,000원 단위여야 합니다."),
-    INVALID_EXCEEDS_MONEY_RANGE("구입 금액은 1,000원 이상이여야 합니다.");
+    INVALID_EXCEEDS_MONEY_RANGE("구입 금액은 1,000원 이상이여야 합니다."),
+    INVALID_WRONG_LOTTO_NUMBER_SIZE("로또 번호는 6자리여야 합니다.")
+    ;
 
 
     private static final String PREFIX = "[ERROR] ";
