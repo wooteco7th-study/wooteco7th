30d0d0eaa85cc529bd3ba91440a57b59705eb5dd
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-17 20:33:55 +0900

    feat(Money): 구입 금액 도메인 구현

---
+++ 7-period/java-lotto-7-main/src/main/java/lotto/domain/Money.java
@@ -0,0 +1,26 @@
+package lotto.domain;
+
+import lotto.error.ErrorMessage;
+import lotto.util.NumberValidator;
+
+public class Money {
+
+    private static final int MIN = 1_000;
+    private static final int MAX = Integer.MAX_VALUE;
+    private static final int UNIT = 1_000;
+    private final int value;
+
+    public Money(final int value) {
+        validate(value);
+        this.value = value;
+    }
+
+    public int calculateQuotient(final int number) {
+        return value / number;
+    }
+
+    private void validate(final int value) {
+        NumberValidator.validateRange(value, MIN, MAX, ErrorMessage.INVALID_EXCEEDS_MONEY_RANGE);
+        NumberValidator.validateUnit(value, UNIT, ErrorMessage.INVALID_WRONG_MONEY_UNIT);
+    }
+}
