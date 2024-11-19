8c5a1bde936a8df771194725bd1c03ec0a09ac46
plan11plan <tjtfy7028@gmail.com>
2024-11-19 22:53:23 +0900

    fix: 검증 실패 문제 해결
    - !빼먹음

--- 6-period/java-baseball-6/src/main/java/baseball/rule/BaseBallValidation.java
+++ 6-period/java-baseball-6/src/main/java/baseball/rule/BaseBallValidation.java
@@ -1,14 +1,11 @@
-package baseball;
+package baseball.rule;
 
-import static baseball.BaseballErrorMessage.*;
-import static baseball.BaseballRule.*;
+import static baseball.rule.BaseballErrorMessage.*;
+import static baseball.rule.BaseballRule.*;
 import static baseball.template.RangeValidator.*;
 
-import baseball.template.RangeValidator;
 import baseball.template.Validator;
 import java.util.List;
-import java.util.stream.Collectors;
-import org.assertj.core.internal.ErrorMessages;
 
 public class BaseBallValidation {
 
@@ -19,12 +16,12 @@ public class BaseBallValidation {
     }
 
     public static void validateThreeSize(List<Integer> numbers){
-        Validator.check(numbers.size() == BASEBALL_SIZE.getValue())
+        Validator.check(numbers.size() != BASEBALL_SIZE.getValue())
                 .withError(new IllegalArgumentException(INVALID_THREE_COUNT_NUMBER.getMessage()))
                 .validate();
     }
     public static void validateInRangeNumber(List<Integer> numbers){
-        numbers.stream().forEach(number -> Validator.check(isNumberInRange(
+        numbers.stream().forEach(number -> Validator.check(!isNumberInRange(
                 number
                 ,MIN_NUMBER.getValue(),
                 MAX_NUMBER.getValue()
