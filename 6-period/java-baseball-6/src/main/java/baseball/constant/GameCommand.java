ee29a08cf23a00a7595e0c634f1f6622158a5c76
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-19 20:46:59 +0900

    feat(GameCommand): 게임 커멘드 정의

---
+++ 6-period/java-baseball-6/src/main/java/baseball/constant/GameCommand.java
@@ -0,0 +1,24 @@
+package baseball.constant;
+
+import baseball.error.AppException;
+import java.util.Arrays;
+import java.util.Objects;
+
+public enum GameCommand {
+
+    RESTART("1"),
+    QUIT("2");
+
+    private final String input;
+
+    GameCommand(final String input) {
+        this.input = input;
+    }
+
+    public static GameCommand findByInput(final String input) {
+        return Arrays.stream(GameCommand.values())
+                .filter(gameCommand -> Objects.equals(gameCommand.input, input))
+                .findAny()
+                .orElseThrow(() -> new AppException(ErrorMessage.INVALID_WRONG_GAME_COMMAND));
+    }
+}
