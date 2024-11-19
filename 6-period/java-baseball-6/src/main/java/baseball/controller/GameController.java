627cf7127677d0f6c9166be553be05b2f6784d05
Jiihyun <cwlgusc@gmail.com>
2024-11-19 23:24:23 +0900

    refactor: 재실행여부 메서드 분리

--- 6-period/java-baseball-6/src/main/java/baseball/controller/GameController.java
+++ 6-period/java-baseball-6/src/main/java/baseball/controller/GameController.java
@@ -3,9 +3,8 @@ package baseball.controller;
 import baseball.domain.GameCommand;
 import baseball.domain.NumberComparator;
 import baseball.domain.Numbers;
-import baseball.domain.RandomNumbersGenerator;
+import baseball.domain.NumbersGenerator;
 import baseball.dto.ResultResponse;
-import baseball.service.GameService;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
@@ -15,38 +14,47 @@ public class GameController {
 
     private final InputView inputView;
     private final OutputView outputView;
+    private final NumbersGenerator numbersGenerator;
 
-    public GameController(final InputView inputView, final OutputView outputView) {
+    public GameController(final InputView inputView, final OutputView outputView, final NumbersGenerator numbersGenerator) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.numbersGenerator = numbersGenerator;
     }
 
     public void run() {
         outputView.printGameStartMsg();
         executeGame();
-        String restartOrNot = inputView.readRestartOrNot();
-        GameCommand gameCommand = GameCommand.from(restartOrNot);
-        if (gameCommand.equals(GameCommand.RESTART)) {
-            executeGame();
-        }
+        restartOrStop();
     }
 
     private void executeGame() {
-        RandomNumbersGenerator randomNumbersGenerator = new RandomNumbersGenerator();
-        Numbers computerNumbers = randomNumbersGenerator.generate();
-//        System.out.println("computerNumbers.getNumbers() = " + computerNumbers.getNumbers());
-        List<Integer> numbers = inputView.readNumbers();
-        Numbers userNumbers = new Numbers(numbers);
+        Numbers computerNumbers = numbersGenerator.generate();
+        Numbers userNumbers = getUserNumbers();
         NumberComparator numberComparator = new NumberComparator();
+
         while (!numberComparator.isThreeStrike(userNumbers, computerNumbers)) {
             ResultResponse resultResponse = numberComparator.compare(userNumbers, computerNumbers);
             outputView.printResult(resultResponse);
             userNumbers = new Numbers(inputView.readNumbers());
         }
-        if (numberComparator.isThreeStrike(userNumbers, computerNumbers)) {
-            ResultResponse resultResponse = numberComparator.compare(userNumbers, computerNumbers);
-            outputView.printResult(resultResponse);
-            outputView.printEndMsg();
+        // FIX: 계산 반복됨
+        ResultResponse resultResponse = numberComparator.compare(userNumbers, computerNumbers);
+        outputView.printResult(resultResponse);
+        outputView.printEndMsg();
+    }
+
+    private Numbers getUserNumbers() {
+        List<Integer> numbers = inputView.readNumbers();
+        return new Numbers(numbers);
+    }
+
+    private void restartOrStop() {
+        String restartOrNot = inputView.readRestartOrNot();
+
+        GameCommand gameCommand = GameCommand.from(restartOrNot);
+        if (gameCommand.equals(GameCommand.RESTART)) {
+            executeGame();
         }
     }
 }
