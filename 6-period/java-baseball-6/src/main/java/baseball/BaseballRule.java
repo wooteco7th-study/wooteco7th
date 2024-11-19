b00a655f690f84743c894c1a6c6b99725269bdc8
plan11plan <tjtfy7028@gmail.com>
2024-11-19 22:19:12 +0900

    refactor: 야구 숫자 규칙 상수 추가

--- 6-period/java-baseball-6/src/main/java/baseball/BaseballRule.java
+++ 6-period/java-baseball-6/src/main/java/baseball/BaseballRule.java
@@ -2,7 +2,9 @@ package baseball;
 
 public enum BaseballRule {
 
-    BASEBALL_SIZE(3);
+    BASEBALL_SIZE(3),
+    MIN_NUMBER(1),
+    MAX_NUMBER(3);
     private final int value;
 
     BaseballRule(final int value) {
