8203a5adba888e7d3f7af60cd10782ac9cb5304c
Mint <audwl03071@gmail.com>
2024-11-19 23:43:29 +0900

    fix: @ValueSource 사용하므로 ParameterizedTest로 수정

--- 6-period/java-baseball-6/src/test/java/baseball/util/InputValidatorTest.java
+++ 6-period/java-baseball-6/src/test/java/baseball/util/InputValidatorTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("입력값 검증 테스트")
@@ -20,7 +21,7 @@ class InputValidatorTest {
                 .hasMessageContaining("null일 수 없습니다.");
     }
 
-    @Test
+    @ParameterizedTest
     @ValueSource(strings = {"", " "})
     @DisplayName("비어있으면 예외가 발생한다.")
     void 실패_검증_empty(String input) {
