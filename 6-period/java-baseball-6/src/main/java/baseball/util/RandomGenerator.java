c805cff4f7e458fa4a1ea55fd17a8f7e6da3f3bb
Mint <audwl03071@gmail.com>
2024-11-19 22:08:18 +0900

    fix: 랜덤 생성시 중복 생성 버그 수정

--- 6-period/java-baseball-6/src/main/java/baseball/util/RandomGenerator.java
+++ 6-period/java-baseball-6/src/main/java/baseball/util/RandomGenerator.java
@@ -9,10 +9,26 @@ public class RandomGenerator {
 
     public static List<Integer> makeRandomNumbers() {
         List<Integer> pickNumbers = new ArrayList<>();
-        while (pickNumbers.stream().distinct().count() != BaseballRules.SIZE.getValue()) {
-            pickNumbers.add(Randoms.pickNumberInRange(BaseballRules.MIN_NUMBER.getValue(),
-                    BaseballRules.MAX_NUMBER.getValue()));
+        while (hasNotEnoughSize(pickNumbers)) {
+            int number = pickNumber();
+            if (doesContains(pickNumbers, number)) {
+                continue;
+            }
+            pickNumbers.add(number);
         }
         return pickNumbers;
+    }
+
+    private static boolean hasNotEnoughSize(final List<Integer> pickNumbers) {
+        return pickNumbers.stream().distinct().count() != BaseballRules.SIZE.getValue();
+    }
+
+    private static int pickNumber() {
+        return Randoms.pickNumberInRange(BaseballRules.MIN_NUMBER.getValue(),
+                BaseballRules.MAX_NUMBER.getValue());
+    }
+
+    private static boolean doesContains(final List<Integer> pickNumbers, final int number) {
+        return pickNumbers.contains(number);
     }
 }
