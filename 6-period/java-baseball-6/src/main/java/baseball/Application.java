29de019e9443ccbd78c8cd162b1dc5bfaebf9898
Mint <audwl03071@gmail.com>
2024-11-19 20:31:13 +0900

    feat: 랜덤 숫자 생성 기능 구현

--- 6-period/java-baseball-6/src/main/java/baseball/Application.java
+++ 6-period/java-baseball-6/src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import baseball.domain.BaseballNumbers;
+import java.util.Set;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+//        ### ✅ 랜덤 숫자 생성하기
+//        1에서 9까지 **서로 다른 임의의 수 3개**를 선택한다.
+        BaseballNumbers numbers = makeNumbers();
+    }
+
+    private static BaseballNumbers makeNumbers() {
+        Set<Integer> pickNumbers = RandomGenerator.makeRandomNumbers();
+        return new BaseballNumbers(pickNumbers);
     }
 }
---
+++ 6-period/java-baseball-6/src/main/java/baseball/RandomGenerator.java
@@ -0,0 +1,16 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashSet;
+import java.util.Set;
+
+public class RandomGenerator {
+
+    public static Set<Integer> makeRandomNumbers() {
+        Set<Integer> pickNumbers = new HashSet<>();
+        while (pickNumbers.size() != 3) {
+            pickNumbers.add(Randoms.pickNumberInRange(1, 9));
+        }
+        return pickNumbers;
+    }
+}
