fe80f0896c513ac20e14eecf949de14cc41c6ae8
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-19 21:54:30 +0900

    feat(GameRule): 게임 룰 정의

---
+++ 6-period/java-baseball-6/src/main/java/baseball/constant/GameRule.java
@@ -0,0 +1,12 @@
+package baseball.constant;
+
+public class GameRule {
+
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 9;
+    public static final int NUMBERS_SIZE = 3;
+
+    private GameRule() {
+
+    }
+}
