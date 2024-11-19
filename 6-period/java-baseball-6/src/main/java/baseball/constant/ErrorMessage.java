7d0d3cea4f6b57587bfbffa4e461b0047b9c58f1
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-19 20:39:34 +0900

    feat(ErrorMessage): 에러 메세지 정의

---
+++ 6-period/java-baseball-6/src/main/java/baseball/constant/ErrorMessage.java
@@ -0,0 +1,20 @@
+package baseball.constant;
+
+public enum ErrorMessage {
+
+    INVALID_WRONG_NUMBER_FORMAT("숫자는 1~9 사이 자연수만 입력 가능합니다."),
+    INVALID_EXCEEDS_NUMBERS_SIZE("숫자는 3자리만 가능합니다."),
+    INVALID_WRONG_GAME_COMMAND("1 또는 2만 입력 가능합니다.");
+
+    private static final String PREFIX = "[ERROR] ";
+
+    private final String message;
+
+    ErrorMessage(final String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
