1c457be205d843a0ddd4e7f692ddd5e44b4f1ad9
Mint <audwl03071@gmail.com>
2024-11-19 21:45:42 +0900

    feat: 결과 출력 기능 구현

--- 6-period/java-baseball-6/src/main/java/baseball/controller/Controller.java
+++ 6-period/java-baseball-6/src/main/java/baseball/controller/Controller.java
@@ -1,21 +1,26 @@
 package baseball.controller;
 
-import baseball.util.RandomGenerator;
+import baseball.domain.BaseballMatcher;
 import baseball.domain.BaseballNumbers;
+import baseball.domain.BaseballResult;
 import baseball.util.Converter;
+import baseball.util.Formatter;
+import baseball.util.RandomGenerator;
 import baseball.view.InputView;
 import baseball.view.OutputView;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
 
     private final InputView inputView;
     private final OutputView outputView;
+    private final Formatter formatter;
 
-    public Controller(final InputView inputView, final OutputView outputView) {
+    public Controller(final InputView inputView, final OutputView outputView, final Formatter formatter) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.formatter = formatter;
     }
 
     public void process() {
@@ -25,7 +30,28 @@ public class Controller {
 //        ### ✅ 숫자 입력하기
 //        -  게임 플레이어는 컴퓨터가 생각하고 있는 **서로 다른 3개의 숫자**를 입력한다.
         outputView.showStartMessage();
+
+        while (true) {
+            BaseballResult result = playGame(numbers);
+            if (result.isWin()) {
+                outputView.showEndMessage();
+                break;
+            }
+        }
+    }
+
+    private BaseballResult playGame(final BaseballNumbers numbers) {
         BaseballNumbers inputNumbers = makeInputNumbers();
+//        ### ✅ 결과 출력하기
+//        - 입력한 수에 대한 결과를 볼, 스트라이크 개수로 표시한다.
+//        - 같은 수가 같은 자리에 있으면 스트라이크,
+//                - 다른 자리에 있으면 볼,
+//                - 같은 수가 전혀 없으면 낫싱
+//        - 맞추지 못할 경우 다시 숫자를 입력받는다.
+        BaseballMatcher matcher = new BaseballMatcher(numbers);
+        BaseballResult result = matcher.match(inputNumbers);
+        outputView.showResult(formatter.makeResult(result));
+        return result;
     }
 
     private BaseballNumbers makeInputNumbers() {
@@ -35,7 +61,7 @@ public class Controller {
     }
 
     private BaseballNumbers makeBaseballNumbers(final String input) {
-        Set<Integer> inputNumbers = new HashSet<>();
+        List<Integer> inputNumbers = new ArrayList<>();
         for (char c : input.toCharArray()) {
             int number = Converter.convertToInteger(c);
             inputNumbers.add(number);
@@ -44,7 +70,7 @@ public class Controller {
     }
 
     private BaseballNumbers makeNumbers() {
-        Set<Integer> pickNumbers = RandomGenerator.makeRandomNumbers();
+        List<Integer> pickNumbers = RandomGenerator.makeRandomNumbers();
         return new BaseballNumbers(pickNumbers);
     }
 }
