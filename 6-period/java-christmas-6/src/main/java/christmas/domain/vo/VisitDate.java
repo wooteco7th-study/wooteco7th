4eb6d9a0555398ea0cf32540e0f54ec62b5acf10
plan11plan <tjtfy7028@gmail.com>
2024-11-23 20:54:48 +0900

    feat: 방문 날짜 범위 검증 기능 구현

--- 6-period/java-christmas-6/src/main/java/christmas/domain/vo/VisitDate.java
+++ 6-period/java-christmas-6/src/main/java/christmas/domain/vo/VisitDate.java
@@ -1,13 +1,21 @@
 package christmas.domain.vo;
 
+import christmas.rule.ChristmasRule;
+import christmas.rule.ErrorMessage;
+import christmas.util.RangeValidator;
+import christmas.view.InputRequestVO.Validator;
 import java.time.LocalDate;
 
 public class VisitDate {
 
     private final LocalDate visitDate;
 
-    public VisitDate(final LocalDate visitDate) {
-        this.visitDate = visitDate;
+    public VisitDate(final int visitDate) {
+        Validator.check(!RangeValidator.isNumberInRange(visitDate,
+                        ChristmasRule.START_DAY.getValue(),
+                        ChristmasRule.END_DAY.getValue()))
+                .withError(new IllegalArgumentException(ErrorMessage.INVALID_RANGE_DATE.getMessage()));
+        this.visitDate = LocalDate.of(2023, 12, visitDate);
     }
 
     public LocalDate getVisitDate() {
