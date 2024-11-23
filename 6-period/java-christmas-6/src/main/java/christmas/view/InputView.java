2ca312a7506ed719547c6b33ecdf173fac2585d7
Jiihyun <cwlgusc@gmail.com>
2024-11-23 14:16:04 +0900

    fix: 방문 날짜 입력메세지 개행 제거

--- 6-period/java-christmas-6/src/main/java/christmas/view/InputView.java
+++ 6-period/java-christmas-6/src/main/java/christmas/view/InputView.java
@@ -12,8 +12,7 @@ public class InputView {
 
     private static final String INPUT_VISIT_DATE_MSG = """
             안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.
-            12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)
-            """;
+            12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)""";
     private static final String INPUT_MENU_AND_QUANTITY_MSG = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
     private static final Pattern ORDER_FORMAT = Pattern.compile("^([가-힣]+-\\d+)(,([가-힣]+-\\d+))*$");
 
