64171e2d6b5730f4cd592df1c00b22cb39269970
Jiihyun <cwlgusc@gmail.com>
2024-11-23 13:59:57 +0900

    feat: 예외 메세지 정의

---
+++ 6-period/java-christmas-6/src/main/java/christmas/exception/ExceptionMessage.java
@@ -0,0 +1,27 @@
+package christmas.exception;
+
+import static java.lang.String.format;
+
+public enum ExceptionMessage {
+
+    INPUT_BLANK("빈 값을 입력하셨습니다."),
+
+    INVALID_NUMBER("유효하지 않은 숫자입니다."),
+
+    INVALID_VISIT_DATE("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
+
+    INVALID_ORDER("유효하지 않은 주문입니다. 다시 입력해 주세요.")
+    ;
+
+    private static final String ERROR_PREFIX = "[ERROR] ";
+
+    private final String message;
+
+    ExceptionMessage(String message) {
+        this.message = ERROR_PREFIX + message;
+    }
+
+    public String getMessage() {
+        return format(message);
+    }
+}
