a6c7e7fb3e83a565aeb44fdb0b00685da0d387da
world-dv <goodforx0147@naver.com>
2024-11-23 16:47:55 +0900

    feat: 메뉴 이름 관련 기능 추가

--- 6-period/java-christmas-6/src/main/java/christmas/Domain/Order.java
+++ 6-period/java-christmas-6/src/main/java/christmas/Domain/Order.java
@@ -1,13 +1,29 @@
 package christmas.Domain;
 
+import java.util.HashMap;
+
 public class Order {
 
+    private final HashMap<String, Integer> menuWithPrice = Menu.getMenuPrice();
+    private final HashMap<String, String> menuWithType = Menu.getMenuType();
     private final String menu;
     private final int count;
 
     public Order(String menu, int count) {
         this.menu = menu;
         this.count = count;
+    }
+
+    public int getPrice() {
+        return menuWithPrice.get(menu) * count;
+    }
+
+    public boolean isDrink() {
+        return menuWithType.get(menu).equals("음료");
+    }
+
+    public String getMenu() {
+        return menu;
     }
 
     public int getCount() {
--- 6-period/java-christmas-6/src/main/java/christmas/Domain/Orders.java
+++ 6-period/java-christmas-6/src/main/java/christmas/Domain/Orders.java
@@ -8,21 +8,44 @@ public class Orders {
 
     public Orders(List<Order> orders) {
         validateTotalCount(orders);
+        validateOnlyDrink(orders);
+        validateMenuDuplicate(orders);
         this.orders = orders;
     }
 
-    //menu 중복 검사
-    //음료만 주문했는지 검사
+    private void validateMenuDuplicate(List<Order> orders) {
+        if (isDuplicate(orders)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isDuplicate(List<Order> orders) {
+        return orders.stream().map(Order::getMenu).distinct().toList().size() != orders.size();
+    }
+
+    private void validateOnlyDrink(List<Order> orders) {
+        if (orders.size() == countDrink(orders)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private int countDrink(List<Order> orders) {
+        return orders.stream().filter(Order::isDrink).toList().size();
+    }
 
     private void validateTotalCount(List<Order> orders) {
-        int totalCount = calculateTotalCount();
+        int totalCount = calculateTotalCount(orders);
         if (totalCount > 20) {
             throw new IllegalArgumentException();
         }
     }
 
-    private int calculateTotalCount() {
+    private int calculateTotalCount(List<Order> orders) {
         return orders.stream().mapToInt(Order::getCount).sum();
+    }
+
+    public int calculateTotalPrice() {
+        return orders.stream().mapToInt(Order::getPrice).sum();
     }
 
     @Override
