0d8f7d4907387da7659abbb58f4c7b43a241dd3a
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-23 18:04:39 +0900

    feat(ChristmasController): 이벤트 응답 부분 구현

--- 6-period/java-christmas-6/src/main/java/christmas/controller/ChristmasController.java
+++ 6-period/java-christmas-6/src/main/java/christmas/controller/ChristmasController.java
@@ -1,8 +1,13 @@
 package christmas.controller;
 
+import christmas.domain.Discount;
+import christmas.domain.EventPlanner;
 import christmas.domain.Order;
 import christmas.domain.OrderGroup;
 import christmas.domain.VisitDate;
+import christmas.dto.BenefitMenuReceipt;
+import christmas.dto.DiscountReceipt;
+import christmas.dto.OrderMenuReceipt;
 import christmas.service.ChristmasService;
 import christmas.util.LoopTemplate;
 import christmas.view.InputView;
@@ -23,12 +28,63 @@ public class ChristmasController {
     }
 
     public void run() {
-        requestVisitDate();
-        requestOrderMenus();
+        outputView.printIntro();
+        final VisitDate visitDate = requestVisitDate();
+        final OrderGroup orderGroup = requestOrderMenus();
+        final List<Discount> discounts = christmasService.generateDiscounts(orderGroup, visitDate);
+        final EventPlanner eventPlanner = new EventPlanner(discounts);
+        responseResult(eventPlanner, orderGroup);
+    }
+
+    private void responseResult(final EventPlanner eventPlanner, final OrderGroup orderGroup) {
+        responseOrderResult(orderGroup);
+        responseBenefitMenuReceipt(eventPlanner);
+        responseDiscountReceipts(eventPlanner);
+        responseTotalDiscount(eventPlanner);
+        responseAfterDiscountTotalPrice(eventPlanner, orderGroup);
+        responseBadgeName(eventPlanner);
+    }
+
+    private void responseBadgeName(final EventPlanner eventPlanner) {
+        final String badgeName = eventPlanner.getBadgeName();
+        outputView.printBadgeName(badgeName);
+    }
+
+    private void responseAfterDiscountTotalPrice(final EventPlanner eventPlanner, final OrderGroup orderGroup) {
+        final long totalPrice = orderGroup.calculateOrdersTotalPrice() - eventPlanner.calculateTotalDiscount();
+        outputView.printAfterDiscountTotalPrice(totalPrice);
+    }
+
+    private void responseTotalDiscount(final EventPlanner eventPlanner) {
+        final int totalDiscount = eventPlanner.calculateTotalDiscount();
+        outputView.printTotalDiscount(totalDiscount);
+    }
+
+    private void responseDiscountReceipts(final EventPlanner eventPlanner) {
+        final List<DiscountReceipt> discountReceipts = christmasService.generateDiscountReceipts(eventPlanner);
+        outputView.printDiscountReceipts(discountReceipts);
+    }
+
+    private void responseBenefitMenuReceipt(final EventPlanner eventPlanner) {
+        final BenefitMenuReceipt benefitMenuReceipt = christmasService.generateBenefitMenuReceipt(eventPlanner);
+        outputView.printBenefitMenuReceipt(benefitMenuReceipt);
+    }
+
+
+    private void responseOrderResult(final OrderGroup orderGroup) {
+        responseOrderReceipts(orderGroup);
+        outputView.printBeforeDiscountTotalPrice(orderGroup.calculateOrdersTotalPrice());
+    }
+
+    private void responseOrderReceipts(final OrderGroup orderGroup) {
+        final List<OrderMenuReceipt> orderMenuReceipts = christmasService.generateOrderMenuReceipts(
+                orderGroup.getOrders());
+        outputView.printOrderMenuReceipts(orderMenuReceipts);
+
     }
 
     private VisitDate requestVisitDate() {
-        return LoopTemplate.tryCatchLoop(() ->{
+        return LoopTemplate.tryCatchLoop(() -> {
             outputView.printAskVisitDay();
             final int day = inputView.readVisitDay();
             return VisitDate.of(day);
