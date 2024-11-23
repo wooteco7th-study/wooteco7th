4803b44b50b3629134c21c32df159b39d46c3c56
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-23 15:37:24 +0900

    refactor(ErrorMessage): 에러 메세지 수정

--- 6-period/java-christmas-6/src/main/java/christmas/error/ErrorMessage.java
+++ 6-period/java-christmas-6/src/main/java/christmas/error/ErrorMessage.java
@@ -2,7 +2,7 @@ package christmas.error;
 
 public enum ErrorMessage {
 
-    INVALID_WRONG_ORDER_FORMAT("유효하지 않은 주문 입니다.다시 입력해 주세요."),
+    INVALID_WRONG_ORDER_FORMAT("유효하지 않은 주문입니다. 다시 입력해 주세요."),
     INVALID_VISIT_DAY_FORMAT("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
 
     private static final String PREFIX = "[ERROR] ";
