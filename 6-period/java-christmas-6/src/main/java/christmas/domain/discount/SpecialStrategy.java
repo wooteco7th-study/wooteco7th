3bc5b5735f9885cba79339f03e4bed668ce9e8f2
Jiihyun <cwlgusc@gmail.com>
2024-11-23 17:04:51 +0900

    feat: 특별 할인 전략 구현

---
+++ 6-period/java-christmas-6/src/main/java/christmas/domain/discount/SpecialStrategy.java
@@ -0,0 +1,27 @@
+package christmas.domain.discount;
+
+import christmas.domain.OrderForm;
+import christmas.domain.VisitDate;
+
+import java.time.LocalDate;
+
+public class SpecialStrategy implements DiscountStrategy {
+
+    private static final int YEAR = 2023;
+    private static final int MONTH = 12;
+    private static final int SUNDAY_VALUE = 7;
+    public static final int CHRISTMAS_DAY_VALUE = 25;
+    private static final int DISCOUNT_AMOUNT = 1000;
+
+    @Override
+    public boolean isApplicable(final VisitDate visitDate) {
+        LocalDate localDate = LocalDate.of(YEAR, MONTH, visitDate.getDate());
+        int value = localDate.getDayOfWeek().getValue();
+        return value == SUNDAY_VALUE || visitDate.getDate() == CHRISTMAS_DAY_VALUE;
+    }
+
+    @Override
+    public int appliedAmount(final OrderForm orderForm) {
+        return DISCOUNT_AMOUNT;
+    }
+}
