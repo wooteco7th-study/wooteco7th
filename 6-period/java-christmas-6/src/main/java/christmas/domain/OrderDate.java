1a1a44c669c4bf0dcd995fc96e8a3046a6323e38
Jiihyun <cwlgusc@gmail.com>
2024-11-23 15:02:12 +0900

    feat: 예상 방문 날짜 범위 검증 - 1~31 사이 범위 아닐 시 예외 처리

---
+++ 6-period/java-christmas-6/src/main/java/christmas/domain/OrderDate.java
@@ -0,0 +1,25 @@
+package christmas.domain;
+
+import static christmas.exception.ExceptionMessage.INVALID_VISIT_DATE;
+
+public class OrderDate {
+
+    public static final int MIN_RANGE = 1;
+    public static final int MAX_RANGE = 31;
+    private final int date;
+
+    public OrderDate(final int date) {
+        validateRange(date);
+        this.date = date;
+    }
+
+    private void validateRange(final int date) {
+        if (date < MIN_RANGE || date > MAX_RANGE) {
+            throw new IllegalArgumentException(INVALID_VISIT_DATE.getMessage());
+        }
+    }
+
+    public int getDate() {
+        return date;
+    }
+}
