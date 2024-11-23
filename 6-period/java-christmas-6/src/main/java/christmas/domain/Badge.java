9216b9b0585334beb5a2fb4d25715a7926cd7445
plan11plan <tjtfy7028@gmail.com>
2024-11-23 15:06:22 +0900

    feat(Badge): 뱃지 반환 기능 구현
    - 산타,트리,별,NONE 뱃지 반환

--- 6-period/java-christmas-6/src/main/java/christmas/domain/Badge.java
+++ 6-period/java-christmas-6/src/main/java/christmas/domain/Badge.java
@@ -1,4 +1,27 @@
 package christmas.domain;
 
+import java.util.Arrays;
+
 public enum Badge {
+
+    SANTA("산타",20_000),
+    TREE("트리",10_000),
+    STAR("별", 5_000),
+    NONE("없음",0);
+
+    private final String badgeName;
+    private final int appliabeCriteriaMoney;
+
+    Badge(final String badgeName, final int appliabeCriteriaMoney) {
+        this.badgeName = badgeName;
+        this.appliabeCriteriaMoney = appliabeCriteriaMoney;
+    }
+
+    public static Badge of(final int totalDiscountAmount){
+       return Arrays.stream(values())
+                .filter(badge -> badge.appliabeCriteriaMoney <= totalDiscountAmount)
+                .findFirst()
+                .orElse(NONE);
+    }
 }
+
