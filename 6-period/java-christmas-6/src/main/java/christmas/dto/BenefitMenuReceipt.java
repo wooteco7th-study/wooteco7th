e4da9d8f8cde05a525ce7a11f63668efcc7c7979
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-23 17:26:50 +0900

    refactor(BenefitMenuReceipt): 증정 메뉴 내역 dto 정팩메 삭제

--- 6-period/java-christmas-6/src/main/java/christmas/dto/BenefitMenuReceipt.java
+++ 6-period/java-christmas-6/src/main/java/christmas/dto/BenefitMenuReceipt.java
@@ -1,13 +1,7 @@
 package christmas.dto;
 
-import christmas.domain.Benefit;
-import christmas.domain.BenefitEvent;
-
 public record BenefitMenuReceipt(
         String name,
         int quantity
 ) {
-    public static BenefitMenuReceipt of(final Benefit benefit) {
-        return new BenefitMenuReceipt(benefit.getBenefitMenuName(), benefit.getBenefitMenuQuantity());
-    }
 }
