d453e9a79715c35147083e7166510fc6cd1c5f83
Mint <audwl03071@gmail.com>
2024-11-23 20:06:31 +0900

    fix: 날짜 포맷팅 버그 수정
    - 날짜별로 출력 메세지 다르게 출력한다.

--- 6-period/java-christmas-6/src/main/java/christmas/view/OutputView.java
+++ 6-period/java-christmas-6/src/main/java/christmas/view/OutputView.java
@@ -7,7 +7,7 @@ public class OutputView {
     private static final String COMMENT_WELCOME_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String COMMENT_VISIT_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     private static final String COMMENT_ORDER_MENU = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)  ";
-    private static final String COMMENT_EVENT = "12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    private static final String COMMENT_EVENT = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
     private static final String ORDER_MENU = "<주문 메뉴>";
     private static final String TOTAL_ORDER_BEFORE_DISCOUNT = "<할인 전 총주문 금액>";
     private static final String BONUS_MENU = "<증정 메뉴>";
@@ -25,8 +25,8 @@ public class OutputView {
         System.out.println(COMMENT_ORDER_MENU);
     }
 
-    public void commentEvent() {
-        System.out.println(COMMENT_EVENT);
+    public void commentEvent(int day) {
+        System.out.printf(String.format(COMMENT_EVENT, day));
         System.out.println();
     }
 
@@ -56,7 +56,7 @@ public class OutputView {
         System.out.println();
     }
 
-    public void showException(Exception exception){
+    public void showException(Exception exception) {
         System.out.println(exception.getMessage());
     }
 }
