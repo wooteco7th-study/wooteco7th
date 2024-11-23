597b1b62d3a2359a3c75254b8065e3cfce491cf5
Jiihyun <cwlgusc@gmail.com>
2024-11-23 14:30:23 +0900

    feat: 메뉴 카테고리 및 메뉴 등록

---
+++ 6-period/java-christmas-6/src/main/java/christmas/domain/Menu.java
@@ -0,0 +1,35 @@
+package christmas.domain;
+
+import static christmas.domain.MenuCategory.APPETIZER;
+import static christmas.domain.MenuCategory.DESSERT;
+import static christmas.domain.MenuCategory.DRINK;
+import static christmas.domain.MenuCategory.MAIN;
+
+public enum Menu {
+
+    MUSHROOM_SOUP(APPETIZER, "양송이수프", 6_000),
+    TAPAS(APPETIZER, "타파스", 5_500),
+    CAESAR_SALAD(APPETIZER, "시저샐러드", 8_000),
+
+    T_BONE_STEAK(MAIN, "티본스테이크", 55_000),
+    BARBECUE_LIP(MAIN, "바비큐립", 54_000),
+    SEAFOOD_PASTA(MAIN, "해산물파스타", 35_000),
+    CHRISTMAS_PASTA(MAIN, "크리스마스파스타", 25_000),
+
+    CHOCO_CAKE(DESSERT, "초코케이크", 15_000),
+    ICE_CREAM(DESSERT, "아이스크림", 5_000),
+
+    ZERO_COKE(DRINK, "제로콜라", 3_000),
+    RED_WINE(DRINK, "레드와인", 60_000),
+    CHAMPAGNE(DRINK, "샴페인", 25_000);
+
+    private MenuCategory category;
+    private String name;
+    private int price;
+
+    Menu(final MenuCategory category, final String name, final int price) {
+        this.category = category;
+        this.name = name;
+        this.price = price;
+    }
+}
