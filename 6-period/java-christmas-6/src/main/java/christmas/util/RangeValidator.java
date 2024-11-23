8729e1f745cb9286480d8c767a9f6cd5e7b789c6
plan11plan <tjtfy7028@gmail.com>
2024-11-23 20:52:38 +0900

    feat: Util 클래스 추가

---
+++ 6-period/java-christmas-6/src/main/java/christmas/util/RangeValidator.java
@@ -0,0 +1,15 @@
+package christmas.util;
+
+public class RangeValidator {
+    private RangeValidator() {
+    }
+
+    public static boolean isNumberInRange(int number, int min, int max) {
+        return number >= min && number <= max;
+    }
+
+    public static boolean isPositive(int number) {
+        return number > 0;
+    }
+
+}
