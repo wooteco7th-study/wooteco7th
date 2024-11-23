c47e8e3d27ecbd2c403a41a713199dbbe5982c68
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-23 17:04:00 +0900

    feat(EventPlanner): 할인 및 이벤트 결과를 연산하는 도메인 구현

---
+++ 6-period/java-christmas-6/src/main/java/christmas/domain/EventPlanner.java
@@ -0,0 +1,54 @@
+package christmas.domain;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class EventPlanner {
+
+
+    public final List<Discount> discounts;
+
+    public EventPlanner(final List<Discount> discounts) {
+        this.discounts = discounts;
+    }
+
+    public int calculateTotalDiscount() {
+        return discounts.stream()
+                .filter(Discount::canReceiveDiscount)
+                .mapToInt(Discount::calculateDiscount)
+                .sum();
+    }
+
+    public String getBenefitMenuName() {
+        return discounts.stream()
+                .filter(Benefit.class::isInstance)
+                .filter(Discount::canReceiveDiscount)
+                .map(discount -> ((BenefitEvent) discount).getBenefitMenuName())
+                .findAny()
+                .orElse(Menu.NONE.getName());
+    }
+
+    public int getBenefitMenuQuantity() {
+        return  discounts.stream()
+                .filter(Benefit.class::isInstance)
+                .filter(Discount::canReceiveDiscount)
+                .map(discount -> ((BenefitEvent) discount).getBenefitMenuQuantity())
+                .findAny()
+                .orElse(0);
+    }
+
+    public Map<DiscountType, Integer> getDiscountResult() {
+        return discounts.stream()
+                .filter(Discount::canReceiveDiscount)
+                .collect(Collectors.toMap(
+                        Discount::getDiscountType,
+                        Discount::calculateDiscount
+                ));
+    }
+
+    public String getBadgeName() {
+        final int totalDiscount = calculateTotalDiscount();
+        return Badge.findByDiscount(totalDiscount).getName();
+    }
+}
