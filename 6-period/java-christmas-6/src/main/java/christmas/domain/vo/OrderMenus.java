21780e050ef7815ec16644fd9c9fba3ce7b2084b
plan11plan <tjtfy7028@gmail.com>
2024-11-23 20:54:13 +0900

    feat: 주문 메뉴 중복 검증 구현

--- 6-period/java-christmas-6/src/main/java/christmas/domain/vo/OrderMenus.java
+++ 6-period/java-christmas-6/src/main/java/christmas/domain/vo/OrderMenus.java
@@ -3,14 +3,17 @@ package christmas.domain.vo;
 import christmas.domain.Discount;
 import christmas.domain.DiscountInfo;
 import christmas.domain.DiscountResult;
+import christmas.rule.ErrorMessage;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class OrderMenus {
     private final Map<Menu, Integer> orderMenus;
 
     public OrderMenus(final Map<Menu, Integer> orderMenus) {
+        validateNoDuplicateMenus(orderMenus);
         this.orderMenus = orderMenus;
     }
 
@@ -38,4 +41,16 @@ public class OrderMenus {
     public int getSize() {
         return orderMenus.size();
     }
+
+    private void validateNoDuplicateMenus(final Map<Menu, Integer> orderMenus) {
+        Set<Menu> uniqueMenus = orderMenus.keySet();
+        Set<String> uniqueMenuNames = uniqueMenus.stream()
+                .map(Menu::getMenuName)
+                .collect(Collectors.toSet());
+
+        if (uniqueMenus.size() != uniqueMenuNames.size()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_DUPLICATE_MENU.getMessage());
+        }
+    }
+
 }
