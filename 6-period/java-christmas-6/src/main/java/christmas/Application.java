9abfa89454d54a5a86823a4823517dd9d7fb8b54
plan11plan <tjtfy7028@gmail.com>
2024-11-24 21:16:14 +0900

    메인에 머지 됐던 부분 수동 삭제

--- 6-period/java-christmas-6/src/main/java/christmas/Application.java
+++ 6-period/java-christmas-6/src/main/java/christmas/Application.java
@@ -1,11 +1,7 @@
 package christmas;
 
-import christmas.controller.OrderController;
 
 public class Application {
     public static void main(String[] args) {
-        
-        OrderController orderController = new OrderController();
-        orderController.run();
     }
 }
--- 6-period/java-christmas-6/src/main/java/christmas/view/InputRequestVO/Delimiter.java
+++
@@ -1,9 +0,0 @@
-package christmas.view.InputRequestVO;
-
-public record Delimiter(
-        String start,      // 시작 구분자 (예: "[", "(", "@")
-        String end,        // 종료 구분자 (예: "]", ")", "@")
-        String pattern,   // 패턴 간 구분자 (예: ";", ",")
-        String value    // 값 구분자 (예: ",", "-")
-) {
-}
