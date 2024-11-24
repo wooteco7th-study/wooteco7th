9f9203f30d7fed13220fc9d03797f05dd64814ce
Mint <audwl03071@gmail.com>
2024-11-24 16:30:26 +0900

    refactor: Badge에 없음 추가
    - enum에 기본값 추가하여 안정성을 높인다.

--- 6-period/java-christmas-6/src/main/java/christmas/domain/Badge.java
+++ 6-period/java-christmas-6/src/main/java/christmas/domain/Badge.java
@@ -5,28 +5,25 @@ import java.util.Arrays;
 
 public enum Badge {
 
-    별("별", new BigDecimal(5000)),
-    트리("트리", new BigDecimal(10000)),
-    산타("산타", new BigDecimal(20000));
+    없음(new BigDecimal(0)),
+    별(new BigDecimal(5000)),
+    트리(new BigDecimal(10000)),
+    산타(new BigDecimal(20000));
 
     public static final String NONE = "없음";
 
-    private final String name;
     private final BigDecimal atLeastPrice;
 
-    Badge(final String name, final BigDecimal atLeastPrice) {
-        this.name = name;
+    Badge(final BigDecimal atLeastPrice) {
         this.atLeastPrice = atLeastPrice;
     }
 
     public static String showName(BigDecimal atLeastPrice) {
-        Badge[] values = Badge.values();
-        Arrays.sort(values, (v1, v2) -> v2.atLeastPrice.compareTo(v1.atLeastPrice));
-        for (Badge badge : values) {
-            if (atLeastPrice.compareTo(badge.atLeastPrice) >= 0) {
-                return badge.name();
-            }
-        }
-        return NONE;
+        return Arrays.stream(Badge.values())
+                .sorted((v1, v2) -> v2.atLeastPrice.compareTo(v1.atLeastPrice))
+                .filter(badge -> atLeastPrice.compareTo(badge.atLeastPrice) >= 0)
+                .findFirst()
+                .map(Badge::name)
+                .orElse(NONE);
     }
 }
