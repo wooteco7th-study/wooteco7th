51a28b24f63d4f616cd9d1a0f1c3223ea56d7577
Mint <audwl03071@gmail.com>
2024-11-24 20:35:33 +0900

    refactor: Menu에 기본타입 추가
    - enum에 기본값 추가하여 안정성을 높인다.

--- 6-period/java-christmas-6/src/main/java/christmas/domain/Menu.java
+++ 6-period/java-christmas-6/src/main/java/christmas/domain/Menu.java
@@ -7,45 +7,41 @@ import java.util.Arrays;
 
 public enum Menu {
 
-    양송이수프("양송이수프", new BigDecimal(6000), MenuType.APPETIZER),
-    타파스("타파스", new BigDecimal(5500), MenuType.APPETIZER),
-    시저샐러드("시저샐러드", new BigDecimal(8000), MenuType.APPETIZER),
+    양송이수프(new BigDecimal(6000), MenuType.APPETIZER),
+    타파스(new BigDecimal(5500), MenuType.APPETIZER),
+    시저샐러드(new BigDecimal(8000), MenuType.APPETIZER),
 
-    티본스테이크("티본스테이크", new BigDecimal(55000), MenuType.MAIN),
-    바비큐립("바비큐립", new BigDecimal(54000), MenuType.MAIN),
-    해산물파스타("해산물파스타", new BigDecimal(35000), MenuType.MAIN),
-    크리스마스파스타("크리스마스파스타", new BigDecimal(25000), MenuType.MAIN),
+    티본스테이크(new BigDecimal(55000), MenuType.MAIN),
+    바비큐립(new BigDecimal(54000), MenuType.MAIN),
+    해산물파스타(new BigDecimal(35000), MenuType.MAIN),
+    크리스마스파스타(new BigDecimal(25000), MenuType.MAIN),
 
-    초코케이크("초코케이크", new BigDecimal(15000), MenuType.DESSERT),
-    아이스크림("아이스크림", new BigDecimal(5000), MenuType.DESSERT),
+    초코케이크(new BigDecimal(15000), MenuType.DESSERT),
+    아이스크림(new BigDecimal(5000), MenuType.DESSERT),
 
-    제로콜라("제로콜라", new BigDecimal(3000), MenuType.DRINK),
-    레드와인("레드와인", new BigDecimal(60000), MenuType.DRINK),
-    샴페인("샴페인", new BigDecimal(25000), MenuType.DRINK);
+    제로콜라(new BigDecimal(3000), MenuType.DRINK),
+    레드와인(new BigDecimal(60000), MenuType.DRINK),
+    샴페인(new BigDecimal(25000), MenuType.DRINK),
 
-    private final String name;
+    없음(BigDecimal.ZERO, MenuType.NONE);
+
     private final BigDecimal price;
     private final MenuType type;
 
-    Menu(final String name, final BigDecimal price, final MenuType type) {
-        this.name = name;
+    Menu(final BigDecimal price, final MenuType type) {
         this.price = price;
         this.type = type;
     }
 
     public static Menu from(String name) {
         return Arrays.stream(Menu.values())
-                .filter(menu -> menu.name.equals(name))
+                .filter(menu -> menu.name().equals(name))
                 .findFirst()
-                .orElseThrow(() -> new CustomIllegalArgumentException(ErrorMessage.INVALID_ORDER.getMessage()));
+                .orElseThrow(() -> new CustomIllegalArgumentException(ErrorMessage.INVALID_ORDER));
     }
 
     public enum MenuType {
-        APPETIZER, MAIN, DESSERT, DRINK;
-    }
-
-    public String getName() {
-        return name;
+        APPETIZER, MAIN, DESSERT, DRINK, NONE;
     }
 
     public BigDecimal getPrice() {
