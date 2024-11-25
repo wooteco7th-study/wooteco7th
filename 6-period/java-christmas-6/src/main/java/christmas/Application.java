431375df72c5d9969853d431a8808019caabf9a1
Mint <audwl03071@gmail.com>
2024-11-26 02:14:09 +0900

    refactor: 사용하지 않는 부분 제거

--- 6-period/java-christmas-6/src/main/java/christmas/Application.java
+++ 6-period/java-christmas-6/src/main/java/christmas/Application.java
@@ -4,7 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 import christmas.controller.Controller;
 import christmas.exception.ExceptionHandler;
 import christmas.service.Service;
-import christmas.support.StringFormatter;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
@@ -13,10 +12,9 @@ public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
-        StringFormatter stringFormatter = new StringFormatter();
         ExceptionHandler exceptionHandler = new ExceptionHandler(outputView);
         Service service = new Service();
-        Controller controller = new Controller(inputView, outputView, stringFormatter, exceptionHandler, service);
+        Controller controller = new Controller(inputView, outputView, exceptionHandler, service);
         try {
             controller.process();
         } finally {
