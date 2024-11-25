261122af12f8767395ac63d13934b6df3b5ff0b6
Mint <audwl03071@gmail.com>
2024-11-26 02:09:57 +0900

    refactor: service에서 도메인 객체 생성 및 dto 생성
    - 비즈니스 로직 처리 제어

--- 6-period/java-christmas-6/src/main/java/christmas/service/Service.java
+++ 6-period/java-christmas-6/src/main/java/christmas/service/Service.java
@@ -5,13 +5,23 @@ import christmas.domain.Menu;
 import christmas.domain.Order;
 import christmas.domain.Orders;
 import christmas.domain.Quantity;
+import christmas.domain.discount.ChristmasDdayDiscount;
+import christmas.domain.discount.DayDiscount;
+import christmas.domain.discount.Discount;
+import christmas.domain.discount.SpecialDiscount;
+import christmas.domain.gift.BonusGift;
+import christmas.domain.gift.Gift;
+import christmas.dto.BenefitDto;
+import christmas.dto.GiftDto;
 import christmas.dto.OrderMenuDto;
 import christmas.exception.CustomIllegalArgumentException;
 import christmas.exception.ErrorMessage;
 import christmas.util.Converter;
 import christmas.util.StringParser;
+import java.util.Collections;
 import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class Service {
 
@@ -47,6 +57,36 @@ public class Service {
     public List<OrderMenuDto> toOrderMenuDto(final Orders orders) {
         return orders.getOrders().stream()
                 .map(OrderMenuDto::of)
+                .toList();
+    }
+
+    public List<Discount> createDiscount(final Day visitDay, final Orders orders) {
+        List<Discount> discounts = List.of(new ChristmasDdayDiscount(visitDay, orders),
+                new DayDiscount(visitDay, orders), new SpecialDiscount(visitDay, orders));
+        return discounts.stream()
+                .filter(Discount::isApplicable)
+                .toList();
+    }
+
+    public List<Gift> createBonus(final Day visitDay, final Orders orders) {
+        Gift gift = new BonusGift(visitDay, orders);
+        if (gift.isApplicable()) {
+            return List.of(gift);
+        }
+        return Collections.emptyList();
+    }
+
+    public List<GiftDto> createGiftDtos(final List<Gift> gifts) {
+        return gifts.stream()
+                .flatMap(gift -> gift.provideGiftItems().entrySet().stream())
+                .map(entry -> new GiftDto(entry.getKey().name(), entry.getValue().getValue()))
+                .toList();
+    }
+
+    public List<BenefitDto> createBenefitDtos(final List<Discount> discounts, final List<Gift> gifts) {
+        return Stream.concat(
+                        discounts.stream().map(discount -> new BenefitDto(discount.getName(), discount.calculateAmount())),
+                        gifts.stream().map(gift -> new BenefitDto(gift.getName(), gift.calculateAmount())))
                 .toList();
     }
 }
