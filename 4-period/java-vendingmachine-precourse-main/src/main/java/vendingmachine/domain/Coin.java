6c074cb5d0db996b6aa14d7eefe104a2ce23f146
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-26 20:38:58 +0900

    feat(Coin): 동전들을 반환 하는 메서드 구현

--- 4-period/java-vendingmachine-precourse-main/src/main/java/vendingmachine/domain/Coin.java
+++ 4-period/java-vendingmachine-precourse-main/src/main/java/vendingmachine/domain/Coin.java
@@ -1,6 +1,8 @@
 package vendingmachine.domain;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public enum Coin {
     COIN_500(500),
@@ -20,5 +22,12 @@ public enum Coin {
                 .filter(coin -> coin.amount == amount)
                 .findAny()
                 .orElse(Coin.NONE);
+    }
+
+    public static List<Coin> findAll() {
+        return Arrays.stream(Coin.values())
+                .sorted((c1, c2) -> c2.amount - c1.amount)
+                .filter(coin -> !Objects.equals(coin, Coin.NONE))
+                .toList();
     }
 }
