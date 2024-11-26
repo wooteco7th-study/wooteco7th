dc58575bca2080b2f5aa5a61f13eebac5aa268a2
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-26 22:47:55 +0900

    feat(ConsoleOutputView): 나머지 출력 메서드 구현

--- 4-period/java-vendingmachine-precourse-main/src/main/java/vendingmachine/view/OutputView.java
+++ 4-period/java-vendingmachine-precourse-main/src/main/java/vendingmachine/view/OutputView.java
@@ -8,4 +8,14 @@ public interface OutputView {
     void printAskVendingMachineMoney();
 
     void printVendingMachineCoins(final Map<Coin, Integer> coins);
+
+    void printAskProducts();
+
+    void printAskInputMoney();
+
+    void printInputMoney(final int money);
+
+    void printAskPurchaseProduct();
+
+    void printRemainingCoins(final Map<Coin, Integer> coins);
 }
