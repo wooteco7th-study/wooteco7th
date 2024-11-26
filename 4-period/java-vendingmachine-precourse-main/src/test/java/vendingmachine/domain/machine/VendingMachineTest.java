fa633a42557810a2e7f438157c9d79de7f6fb729
plan11plan <tjtfy7028@gmail.com>
2024-11-27 00:03:03 +0900

    test: 자판기 상품 초기화 테스트

--- 4-period/java-vendingmachine-precourse-main/src/test/java/vendingmachine/domain/machine/VendingMachineTest.java
+++ 4-period/java-vendingmachine-precourse-main/src/test/java/vendingmachine/domain/machine/VendingMachineTest.java
@@ -1,10 +1,12 @@
 package vendingmachine.domain.machine;
 
 import java.util.Map;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import vendingmachine.domain.money.Coin;
 import vendingmachine.domain.money.Money;
+import vendingmachine.domain.product.Product;
 
 class VendingMachineTest {
 
@@ -21,5 +23,36 @@ class VendingMachineTest {
         // then
         System.out.println(coinStatus);
     }
+
+    @DisplayName("상품 1개 저장 기능 작동 테스트")
+    @Test
+    void 상품_1개_정상_저장() {
+        // given
+        Money inputMoney = Money.of(3000);
+        VendingMachine vendingMachine = new VendingMachine(inputMoney);
+        Product product = Product.create("콜라", Money.of(3000), 10);
+
+        // when
+        Assertions.assertThatCode(() -> vendingMachine.addProduct(product))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("중복 상품 예외 반환 테스트")
+    @Test
+    void 중복_상품_저장_예외_반환() {
+        // given
+        Money inputMoney = Money.of(3000);
+        VendingMachine vendingMachine = new VendingMachine(inputMoney);
+        Product product1 = Product.create("콜라", Money.of(3000), 10);
+        vendingMachine.addProduct(product1);
+
+        // when
+        Product product2 = Product.create("콜라", Money.of(3000), 10);
+
+        // then
+        Assertions.assertThatThrownBy(() -> vendingMachine.addProduct(product2))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 
 }
