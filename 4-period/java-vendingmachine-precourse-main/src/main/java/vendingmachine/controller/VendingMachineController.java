2821faf127ce6b6ae17f0a2885de8c2fb0c872db
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-26 22:59:40 +0900

    feat(VendingMachineController): 상품 및 투입 금액 요청, 구매상품 요청, 투입 금액 응답, 잔돈 응답 구현

--- 4-period/java-vendingmachine-precourse-main/src/main/java/vendingmachine/controller/VendingMachineController.java
+++ 4-period/java-vendingmachine-precourse-main/src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -1,9 +1,15 @@
 package vendingmachine.controller;
 
+import java.util.List;
 import java.util.Map;
 import vendingmachine.domain.Coin;
+import vendingmachine.domain.CoinGroup;
 import vendingmachine.domain.CoinsConvertor;
 import vendingmachine.domain.Money;
+import vendingmachine.domain.Product;
+import vendingmachine.domain.ProductGroup;
+import vendingmachine.domain.ProductsGenerator;
+import vendingmachine.domain.VendingMachine;
 import vendingmachine.util.LoopTemplate;
 import vendingmachine.view.InputView;
 import vendingmachine.view.OutputView;
@@ -19,10 +25,59 @@ public class VendingMachineController {
     }
 
     public void run() {
-        final Money money = requestVendingMachineMoney();
-        final Map<Coin, Integer> coins = CoinsConvertor.convert(money);
+        final Money vendingMachineMoney = requestVendingMachineMoney();
+        final Map<Coin, Integer> coins = CoinsConvertor.convert(vendingMachineMoney);
         outputView.printVendingMachineCoins(coins);
+        final VendingMachine vendingMachine = generateVendingMachine(coins);
+        purchase(vendingMachine);
     }
+
+    private void purchase(final VendingMachine vendingMachine) {
+        while (vendingMachine.hasPurchaseAbleProduct()) {
+            purchaseProduct(vendingMachine);
+        }
+        responseRemainingCoins(vendingMachine);
+    }
+
+    private void responseRemainingCoins(final VendingMachine vendingMachine) {
+        outputView.printInputMoney(vendingMachine.getMoneyValue());
+        outputView.printRemainingCoins(vendingMachine.getRemainingCoins());
+    }
+
+    private void purchaseProduct(final VendingMachine vendingMachine) {
+        LoopTemplate.tryCatchLoop(() -> {
+            outputView.printInputMoney(vendingMachine.getMoneyValue());
+            outputView.printAskPurchaseProduct();
+            final String name = inputView.purchaseProduct();
+            vendingMachine.purchaseProduct(name);
+        });
+    }
+
+
+    private VendingMachine generateVendingMachine(final Map<Coin, Integer> coins) {
+        final CoinGroup coinGroup = new CoinGroup(coins);
+        final ProductGroup productGroup = requestProducts();
+        final Money inputMoney = requestInputMoney();
+        return new VendingMachine(coinGroup, productGroup, inputMoney);
+    }
+
+    private Money requestInputMoney() {
+        return LoopTemplate.tryCatchLoop(() -> {
+            outputView.printAskInputMoney();
+            final int money = inputView.readMoney();
+            return new Money(money);
+        });
+    }
+
+    private ProductGroup requestProducts() {
+        return LoopTemplate.tryCatchLoop(() -> {
+            outputView.printAskProducts();
+            final List<String> inputs = inputView.readProducts();
+            final List<Product> products = ProductsGenerator.generate(inputs);
+            return new ProductGroup(products);
+        });
+    }
+
 
     private Money requestVendingMachineMoney() {
         return LoopTemplate.tryCatchLoop(() -> {
@@ -31,5 +86,4 @@ public class VendingMachineController {
             return new Money(money);
         });
     }
-
 }
