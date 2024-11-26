975390c0308752e9b7be90784da36cf1c6b64f0f
world-dv <goodforx0147@naver.com>
2024-11-26 20:07:41 +0900

    feat: 출력 메시지 정의 및 view 정의

---
+++ 4-period/java-vendingmachine-precourse-main/src/main/java/vendingmachine/view/OutputView.java
@@ -0,0 +1,8 @@
+package vendingmachine.view;
+
+public class OutputView {
+
+    public static void printMessage(PrintMessage printMessage) {
+        System.out.println(printMessage.getMessage());
+    }
+}
