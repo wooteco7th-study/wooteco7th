6e2843b9055b249d05f7eb7a37a4783484bc6a4d
plan11plan <tjtfy7028@gmail.com>
2024-11-26 23:35:15 +0900

    feat: 리드미에 적은 역할들 뼈대 구현

---
+++ 4-period/java-vendingmachine-precourse-main/src/main/java/vendingmachine/role/MoneyManager.java
@@ -0,0 +1,15 @@
+package vendingmachine.role;
+
+/*
+
+> 투입 금액으로는 동전을 생성하지 않는다.
+
+- [ ] 금액을 추가한다.
+- [ ] 주어진 금액만큼의 잔액이 있는지 확인한다.
+- [ ] 금액을 차감한다
+    - 잔액 부족시 (throw Exception)
+- [ ] 현재 잔액을 조회한다.
+
+ */
+public interface MoneyManager {
+}
---
+++ 4-period/java-vendingmachine-precourse-main/src/main/java/vendingmachine/role/ProductSeller.java
@@ -0,0 +1,10 @@
+package vendingmachine.role;
+
+/*
+
+- [ ] 상품을 판매하고 결과를 반환한다.
+    - 상품 품절 시 (throw Exception)
+    - 잔액 부족 시 (throw Exception)
+ */
+public interface ProductSeller {
+}
