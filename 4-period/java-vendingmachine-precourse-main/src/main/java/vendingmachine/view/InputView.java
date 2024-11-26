1e6e49bdb09deb7ee3dc2855383417c8be740d53
plan11plan <tjtfy7028@gmail.com>
2024-11-26 21:25:10 +0900

    feat: 입력 뷰 기능 구현
    - 자판기 금액 입력 기능
    - 자판기 상품 입력 기능
    - 사용자 금액 입력 기능
    - 사용자 상품명 입력 기능
    - 에러 메시지 정의
    - 상풍 요청 DTO 객체 정의
    - Money 값 객체 구현

---
+++ 4-period/java-vendingmachine-precourse-main/src/main/java/vendingmachine/view/InputView.java
@@ -0,0 +1,94 @@
+package vendingmachine.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import vendingmachine.ErrorMessage;
+import vendingmachine.Money;
+
+/**
+ * (인풋) 자판기가 보유하고 있는 금액을 입력해 주세요. 450
+ * <p>
+ * (인풋) 상품명과 가격, 수량을 입력해 주세요. [콜라,1500,20];[사이다,1000,10]
+ * <p>
+ * (인풋) 투입 금액을 입력해 주세요. 3000
+ * <p>
+ * (인풋) 구매할 상품명을 입력해 주세요. 콜라
+ */
+public class InputView {
+
+    private static final String NEW_LINE = "\n";
+    private static final String INITIAL_AMOUNT_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+    private static final String PRODUCT_REGISTRATION_MESSAGE = NEW_LINE + "상품명과 가격, 수량을 입력해 주세요.";
+    private static final String INSERT_MONEY_MESSAGE = NEW_LINE + "투입 금액을 입력해 주세요.";
+    private static final String PRODUCT_NAME_MESSAGE = "구매할 상품명을 입력해 주세요.";
+    //
+
+    /**
+     * (인풋) * 자판기가 보유하고 있는 금액을 입력해 주세요. * 450
+     */
+    public Money requestInitialAmount() {
+        println(INITIAL_AMOUNT_MESSAGE);
+        try {
+            return Money.of(Integer.parseInt(readLine().trim()));
+        } catch (Exception e) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_REQUEST_AMOUNT.getMessage());
+        }
+    }
+
+    /**
+     * (인풋) * 상품명과 가격, 수량을 입력해 주세요. * [콜라,1500,20];[사이다,1000,10]
+     */
+    public List<ProductRegistrationRequest> requestProductRegistration() {
+        println(PRODUCT_REGISTRATION_MESSAGE);
+        String input = readLine().trim();
+        try {
+            return ProductRegistrationRequest.from(input);
+
+        } catch (Exception e) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_REQUEST_PRODUCT_FORM.getMessage());
+        }
+    }
+
+
+    /**
+     * (인풋) * 투입 금액을 입력해 주세요. * 3000
+     */
+    public Money requestUserMoney() {
+        println(INSERT_MONEY_MESSAGE);
+        try {
+            String input = Console.readLine();
+            int amount = Integer.parseInt(input);
+            validateAmount(amount);
+            return Money.of(amount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_REQUEST_AMOUNT.getMessage());
+        }
+    }
+
+    /**
+     * (인풋) * 구매할 상품명을 입력해 주세요. * 콜라
+     */
+    public String readProductName() {
+        System.out.println(PRODUCT_NAME_MESSAGE);
+        String input = Console.readLine();
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_REQUEST_PRODUCT_NAME.getMessage());
+        }
+        return input;
+    }
+    
+    private void validateAmount(int amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_REQUEST_AMOUNT_RANGE.getMessage());
+        }
+        if (amount % 10 != 0) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_REQUEST_AMOUNT_UNIT.getMessage());
+        }
+    }
+
+    public void println(String message) {
+        System.out.println(message);
+    }
+}
---
+++ 4-period/java-vendingmachine-precourse-main/src/main/java/vendingmachine/view/ProductRegistrationRequest.java
@@ -0,0 +1,60 @@
+package vendingmachine.view;
+
+import static vendingmachine.ErrorMessage.INVALID_PARSE_TO_INT;
+import static vendingmachine.ErrorMessage.INVALID_REQUEST_PRODUCT_FORM;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ProductRegistrationRequest {
+    private final static int DEFAULT_SIZE = 3;
+    private final static String SPLIT_COMMA = ",";
+    private final static String SPLIT_SEMICOLON = ",";
+    private final String name;
+    private final int price;
+    private final int quantity;
+
+    private ProductRegistrationRequest(String name, int price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public static List<ProductRegistrationRequest> from(String input) {
+
+        String sanitizedInput = input.replaceAll("\\[|\\]", "").trim();
+        String[] productEntries = sanitizedInput.split(SPLIT_SEMICOLON);
+
+        return Arrays.stream(productEntries)
+                .map(String::trim)
+                .filter(entry -> !entry.isEmpty())
+                .map(entry -> {
+                    String[] productInfo = entry.split(SPLIT_COMMA);
+                    validateProductInfo(productInfo);
+                    return new ProductRegistrationRequest(
+                            productInfo[0].trim(),
+                            parseToInt(productInfo[1]),
+                            parseToInt(productInfo[2])
+                    );
+                })
+                .collect(Collectors.toList());
+    }
+
+    private static void validateProductInfo(String[] productInfo) {
+        if (productInfo.length != DEFAULT_SIZE) {
+            throw new IllegalArgumentException(INVALID_REQUEST_PRODUCT_FORM.getMessage());
+        }
+
+    }
+
+    private static int parseToInt(String value) {
+        try {
+            return Integer.parseInt(value.trim());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_PARSE_TO_INT.getMessage());
+        }
+    }
+
+
+}
