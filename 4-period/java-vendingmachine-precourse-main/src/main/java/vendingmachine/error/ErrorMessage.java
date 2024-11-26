4972e8aa1d5d33432c0b5908490a72b1f4d8e4db
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-26 23:12:46 +0900

    fix: 예외 테스트 실패하는 부분 수정

--- 4-period/java-vendingmachine-precourse-main/src/main/java/vendingmachine/error/ErrorMessage.java
+++ 4-period/java-vendingmachine-precourse-main/src/main/java/vendingmachine/error/ErrorMessage.java
@@ -7,7 +7,8 @@ public enum ErrorMessage {
     INVALID_DUPLICATED_PRODUCT("상품은 중복될 수 없습니다."),
     INVALID_PRODUCT_PRICE("상품 가격은 100원 이상 10원 단위여야 합니다."),
     INVALID_PRODUCT_QUANTITY("상품 수량은 1개이상이여야 합니다."),
-    INVALID_CAN_NOT_PURCHASE_PRODUCT("구매할 수 없는 상품 입니다.");
+    INVALID_CAN_NOT_PURCHASE_PRODUCT("구매할 수 없는 상품 입니다."),
+    INVALID_EXCEEDS_MONEY_RANGE("금액은 자연수여야 합니다.");
 
     private static final String PREFIX = "[ERROR] ";
     private final String message;
