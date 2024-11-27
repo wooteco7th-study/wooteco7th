a31dcebf2d370dd969ac0104350c784bcdabb866
Jiihyun <cwlgusc@gmail.com>
2024-11-27 16:05:44 +0900

    refactor: 컨트롤러 메서드 책임에 따라 분리

--- 4-period/java-vendingmachine-precourse-main/src/main/java/vendingmachine/controller/MachineController.java
+++ 4-period/java-vendingmachine-precourse-main/src/main/java/vendingmachine/controller/MachineController.java
@@ -20,26 +20,22 @@ public class MachineController {
     }
 
     public void run() {
+        MachineCoins machineCoins = saveMachineCoins();
+
+        Inventories inventories = retryOnInvalidInput(inputView::readInventories);
+        int orderAmount = retryOnInvalidInput(inputView::readOrderAmount);
+        orderAmount = processOrders(orderAmount, inventories);
+
+        Map<Coin, Integer> change = machineCoins.update(orderAmount);
+        outputView.printOrderAmount(orderAmount);
+        outputView.printChange(change);
+    }
+
+    private MachineCoins saveMachineCoins() {
         MachineCoins machineCoins = retryOnInvalidInput(this::getMachineCoins);
         machineCoins.saveCoins();
         outputView.printMachineCoins(machineCoins.toResponse());
-        Inventories inventories = retryOnInvalidInput(inputView::readInventories);
-        int orderAmount = retryOnInvalidInput(inputView::readOrderAmount);
-
-        do {
-            outputView.printOrderAmount(orderAmount);
-            final int currentOrderAmount = orderAmount;
-            orderAmount = retryOnInvalidInput(() -> getOrderAmount(currentOrderAmount, inventories));
-        } while (inventories.keepOrder(orderAmount));
-
-        Map<Coin, Integer> coins = machineCoins.update(orderAmount);
-        outputView.printOrderAmount(orderAmount);
-        outputView.printChange(coins);
-    }
-
-    private int getOrderAmount(int orderAmount, final Inventories inventories) {
-        String orderProductName = inputView.readOrderProductName();
-        return inventories.update(orderAmount, orderProductName);
+        return machineCoins;
     }
 
     private MachineCoins getMachineCoins() {
@@ -47,11 +43,26 @@ public class MachineController {
         return new MachineCoins(amountHeld);
     }
 
+    private int processOrders(int orderAmount, final Inventories inventories) {
+        do {
+            outputView.printOrderAmount(orderAmount);
+            final int currentOrderAmount = orderAmount;
+            orderAmount = retryOnInvalidInput(() -> getOrderAmount(currentOrderAmount, inventories));
+        } while (inventories.keepOrder(orderAmount));
+        return orderAmount;
+    }
+
+    private int getOrderAmount(int orderAmount, final Inventories inventories) {
+        String orderProductName = inputView.readOrderProductName();
+        return inventories.update(orderAmount, orderProductName);
+    }
+
     private <T> T retryOnInvalidInput(Supplier<T> input) {
         while (true) {
             try {
                 return input.get();
             } catch (IllegalArgumentException e) {
+                // Q: retryHandler 클래스 빼면 output의존성 주입해야하는데 귀찮음(일을 키우는 느낌)
                 outputView.printError(e.getMessage());
             }
         }
