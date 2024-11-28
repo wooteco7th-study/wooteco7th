0f2a6347956812b31ef1d141171c834b36e85df4
Mint <audwl03071@gmail.com>
2024-11-29 00:23:15 +0900

    refactor: LinkedHashMap 사용하여 순서 유지

--- 4-period/java-vendingmachine-precourse-main/src/main/java/vendingmachine/domain/price/coin/RandomCoinGenerator.java
+++ 4-period/java-vendingmachine-precourse-main/src/main/java/vendingmachine/domain/price/coin/RandomCoinGenerator.java
@@ -1,16 +1,17 @@
 package vendingmachine.domain.price.coin;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import vendingmachine.domain.price.Price;
 
 public class RandomCoinGenerator implements CoinGenerator {
 
     @Override
     public Map<Coin, Integer> generateCoins(final Price price) {
-        Map<Coin, Integer> coins = new HashMap<>();
+        Map<Coin, Integer> coins = initializeMaps();
         int target = price.getAmount();
         while (target >= Coin.getLowestAmount()) {
             int pickAmount = pickCoinAmount(target);
@@ -18,6 +19,16 @@ public class RandomCoinGenerator implements CoinGenerator {
             coins.merge(Coin.createCoin(pickAmount), 1, Integer::sum);
         }
         return coins;
+    }
+
+    private Map<Coin, Integer> initializeMaps() {
+        return Coin.sortedCoins().stream()
+                .collect(Collectors.toMap(
+                        coin -> coin,
+                        quantity -> 0,
+                        (oldValue, newValue) -> oldValue,
+                        LinkedHashMap::new
+                ));
     }
 
     private int pickCoinAmount(final int target) {
