6481b4c71d1b3d2f65785ca610a8c509a359e9f0
Mint <audwl03071@gmail.com>
2024-11-28 21:50:24 +0900

    refactor: enum에 필드 추가
    - none 제외 정렬된 필드 추가하여 사용하기 쉽도록 한다.

--- 4-period/java-vendingmachine-precourse-main/src/main/java/vendingmachine/domain/price/coin/Coin.java
+++ 4-period/java-vendingmachine-precourse-main/src/main/java/vendingmachine/domain/price/coin/Coin.java
@@ -3,7 +3,6 @@ package vendingmachine.domain.price.coin;
 import static vendingmachine.exception.ErrorMessage.COIN_NOT_EXIST;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import vendingmachine.domain.price.Price;
 import vendingmachine.exception.CustomIllegalArgumentException;
@@ -13,7 +12,13 @@ public enum Coin {
     COIN_500(new Price(500)),
     COIN_100(new Price(100)),
     COIN_50(new Price(50)),
-    COIN_10(new Price(10));
+    COIN_10(new Price(10)),
+    COIN_0(new Price(0));
+
+    private static final List<Coin> VALUED_COINS = Arrays.stream(Coin.values())
+            .filter(coin -> coin != COIN_0)
+            .sorted((c1, c2) -> Long.compare(c2.getPrice().getAmount(), c1.getPrice().getAmount()))
+            .toList();
 
     private final Price price;
 
@@ -22,28 +27,24 @@ public enum Coin {
     }
 
     public static Coin createCoin(Price price) {
-        return Arrays.stream(Coin.values())
+        return VALUED_COINS.stream()
                 .filter(coin -> coin.getPrice().equals(price))
                 .findFirst()
                 .orElseThrow(() -> new CustomIllegalArgumentException(COIN_NOT_EXIST));
     }
 
     public static List<Coin> calculateAvailableCoinTypes(Price price) {
-        return Arrays.stream(Coin.values())
+        return VALUED_COINS.stream()
                 .filter(coin -> price.isMoreThanEqual(coin.getPrice()))
                 .toList();
     }
 
     public static List<Coin> sortedCoins() {
-        return Arrays.stream(Coin.values())
-                .sorted((coin1, coin2) -> Long.compare(coin2.getPrice().getAmount(), coin1.getPrice().getAmount()))
-                .toList();
+        return VALUED_COINS;
     }
 
     public static Coin getLowest() {
-        return Arrays.stream(Coin.values())
-                .min(Comparator.comparingLong(c -> c.getPrice().getAmount()))
-                .orElseThrow(() -> new CustomIllegalArgumentException(COIN_NOT_EXIST));
+        return VALUED_COINS.getLast();
     }
 
     public Price getPrice() {
