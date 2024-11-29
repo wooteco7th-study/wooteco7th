b088aef4cebafac38a44711ca90b28575182a932
Mint <audwl03071@gmail.com>
2024-11-29 18:40:37 +0900

    refactor: 조건문 명확하게 수정

--- 4-period/java-vendingmachine-precourse-main/src/main/java/vendingmachine/domain/price/HoldingPrice.java
+++ 4-period/java-vendingmachine-precourse-main/src/main/java/vendingmachine/domain/price/HoldingPrice.java
@@ -1,7 +1,7 @@
 package vendingmachine.domain.price;
 
 import static vendingmachine.exception.ErrorMessage.INVALID_PRICE_DIVIDE_BY_TEN;
-import static vendingmachine.exception.ErrorMessage.INVALID_PRICE_UNDER_TEN;
+import static vendingmachine.exception.ErrorMessage.INVALID_PRICE_ZERO;
 
 import vendingmachine.exception.CustomIllegalArgumentException;
 
@@ -13,8 +13,8 @@ public class HoldingPrice extends Price {
     }
 
     private void validate(final int amount) {
-        if (amount < 10) {
-            throw new CustomIllegalArgumentException(INVALID_PRICE_UNDER_TEN);
+        if (amount == 0) {
+            throw new CustomIllegalArgumentException(INVALID_PRICE_ZERO);
         }
         if (amount % 10 != 0) {
             throw new CustomIllegalArgumentException(INVALID_PRICE_DIVIDE_BY_TEN);
