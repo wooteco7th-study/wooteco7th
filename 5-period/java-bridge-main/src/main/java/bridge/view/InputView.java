65d52c9c253e66d9444c4e0cfe1db9dcdd2d9d01
Jiihyun <cwlgusc@gmail.com>
2024-11-30 14:06:07 +0900

    refactor: 입력 메세지 개행 제거

--- 5-period/java-bridge-main/src/main/java/bridge/view/InputView.java
+++ 5-period/java-bridge-main/src/main/java/bridge/view/InputView.java
@@ -14,8 +14,7 @@ public class InputView {
     private static final String BRIDGE_SIZE_MSG = """
             다리 건너기 게임을 시작합니다.
             
-            다리의 길이를 입력해주세요.
-            """;
+            다리의 길이를 입력해주세요.""";
     private static final String MOVING_MSG = NEW_LINE + "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String GAME_COMMAND_MSG = NEW_LINE + "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
