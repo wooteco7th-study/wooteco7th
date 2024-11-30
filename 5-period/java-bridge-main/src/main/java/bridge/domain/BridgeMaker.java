c35463cbcd2ce1d36174813ce3e56772c298a7f9
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-30 14:09:09 +0900

    feat(BridgeMaker): 다리 메이커 구현

--- 5-period/java-bridge-main/src/main/java/bridge/domain/BridgeMaker.java
+++ 5-period/java-bridge-main/src/main/java/bridge/domain/BridgeMaker.java
@@ -1,11 +1,17 @@
 package bridge.domain;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
+
+    private static final String UP = "U";
+    private static final String DOWN = "D";
+    private static final int UP_NUMBER = 0;
+    private static final int DOWN_NUMBER = 1;
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
@@ -18,6 +24,18 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        return IntStream.range(0, size)
+                .mapToObj(idx -> getBridge(bridgeNumberGenerator.generate()))
+                .toList();
+    }
+
+    private String getBridge(final int number) {
+        if (number == UP_NUMBER) {
+            return UP;
+        }
+        if (number == DOWN_NUMBER) {
+            return DOWN;
+        }
+        return "";
     }
 }
