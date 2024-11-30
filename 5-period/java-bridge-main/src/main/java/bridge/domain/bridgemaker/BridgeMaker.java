4651df3f7284f028b3d0ef79ee3d5a70b94b8596
Jiihyun <cwlgusc@gmail.com>
2024-11-30 14:14:05 +0900

    feat: 다리 길이 예외 사항 추가 - 다리 길이 3에서 20사이 아닐 시 예외 처리

--- 5-period/java-bridge-main/src/main/java/bridge/domain/bridgemaker/BridgeMaker.java
+++ 5-period/java-bridge-main/src/main/java/bridge/domain/bridgemaker/BridgeMaker.java
@@ -5,11 +5,16 @@ import bridge.domain.GameCommand;
 import java.util.ArrayList;
 import java.util.List;
 
+import static bridge.exception.ExceptionMessage.SIZE_OUT_OF_RANGE;
+
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
 
+    public static final int MIN_SIZE = 3;
+    public static final int MAX_SIZE = 20;
+    
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
@@ -21,6 +26,17 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
+        validateSize(size);
+        return createBridgeAnswer(size);
+    }
+
+    private void validateSize(int size) {
+        if (size < MIN_SIZE || size > MAX_SIZE) {
+            throw new IllegalArgumentException(SIZE_OUT_OF_RANGE.getMessage());
+        }
+    }
+
+    private List<String> createBridgeAnswer(final int size) {
         List<String> bridgeAnswer = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             int randomNumber = bridgeNumberGenerator.generate();
--- 5-period/java-bridge-main/src/main/java/bridge/exception/ExceptionMessage.java
+++ 5-period/java-bridge-main/src/main/java/bridge/exception/ExceptionMessage.java
@@ -1,5 +1,7 @@
 package bridge.exception;
 
+import static bridge.domain.bridgemaker.BridgeMaker.MAX_SIZE;
+import static bridge.domain.bridgemaker.BridgeMaker.MIN_SIZE;
 import static java.lang.String.format;
 
 public enum ExceptionMessage {
@@ -8,6 +10,11 @@ public enum ExceptionMessage {
     INVALID_NUMBER("유효하지 않은 숫자입니다."),
 
     INVALID_FORMAT("유효하지 않은 포맷입니다."),
+
+    SIZE_OUT_OF_RANGE(
+            format("다리 길이는 %d에서 %d사이여야 합니다.",
+                    MIN_SIZE, MAX_SIZE
+            )),
 
     COMMAND_NOT_FOUND("존재하지 않는 게임 명령어 입니다."),
 
