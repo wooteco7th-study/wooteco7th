d6edeea2fe1eafbd8344e8561914a3751966ea2c
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-30 16:58:25 +0900

    test(BridgeGameTest): BridgeLog 추상화에 따른 테스트 코드 수정

--- 5-period/java-bridge-main/src/test/java/bridge/domain/BridgeGameTest.java
+++ 5-period/java-bridge-main/src/test/java/bridge/domain/BridgeGameTest.java
@@ -7,35 +7,34 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BridgeGameTest {
-    
+
     @Test
     @DisplayName("다리길이가 3, 모두 위 다리로 이동 가능 할때 turn 3회 모두 위 다리로 이동하여 게임이 끝난다.")
     void true_endTest() throws Exception {
         //given
         final Bridge bridge = new Bridge(List.of(MoveCommand.UP, MoveCommand.UP, MoveCommand.UP));
-        final UpBridgeLog upBridgeLog = new UpBridgeLog();
-        final DownBridgeLog downBridgeLog = new DownBridgeLog();
+        final BridgeLogGroup bridgeLogGroup = BridgeLogsGenerator.generate();
 
         //when
-        final BridgeGame bridgeGame = new BridgeGame(downBridgeLog, upBridgeLog, bridge);
+        final BridgeGame bridgeGame = new BridgeGame(bridgeLogGroup, bridge);
         bridgeGame.move(MoveCommand.UP);
         bridgeGame.move(MoveCommand.UP);
         bridgeGame.move(MoveCommand.UP);
 
         //then
         assertThat(bridgeGame.isClear()).isTrue();
-        
+
     }
+
     @Test
     @DisplayName("다리길이가 3, 모두 위 다리로 이동 가능 할때 turn 3회 모두 위 다리로 이동하지 않아 게임이 끝나지 않는다.")
     void false_endTest() throws Exception {
         //given
         final Bridge bridge = new Bridge(List.of(MoveCommand.UP, MoveCommand.UP, MoveCommand.UP));
-        final UpBridgeLog upBridgeLog = new UpBridgeLog();
-        final DownBridgeLog downBridgeLog = new DownBridgeLog();
+        final BridgeLogGroup bridgeLogGroup = BridgeLogsGenerator.generate();
 
         //when
-        final BridgeGame bridgeGame = new BridgeGame(downBridgeLog, upBridgeLog, bridge);
+        final BridgeGame bridgeGame = new BridgeGame(bridgeLogGroup, bridge);
         bridgeGame.move(MoveCommand.DOWN);
         bridgeGame.move(MoveCommand.UP);
         bridgeGame.move(MoveCommand.DOWN);
