ea030746301fe0ca7f3a420981c20c40aa16d589
Jiihyun <cwlgusc@gmail.com>
2024-11-30 13:47:13 +0900

    feat: 에러 메세지 정의

---
+++ 5-period/java-bridge-main/src/main/java/bridge/exception/ExceptionMessage.java
@@ -0,0 +1,25 @@
+package bridge.exception;
+
+import static java.lang.String.format;
+
+public enum ExceptionMessage {
+    INPUT_BLANK("빈 값을 입력하셨습니다."),
+
+    INVALID_NUMBER("유효하지 않은 숫자입니다."),
+
+    INVALID_FORMAT("유효하지 않은 포맷입니다."),
+
+
+    ;
+    private static final String ERROR_PREFIX = "[ERROR] ";
+
+    private final String message;
+
+    ExceptionMessage(String message) {
+        this.message = ERROR_PREFIX + message;
+    }
+
+    public String getMessage() {
+        return format(message);
+    }
+}
