305546316d789869942f5178fc0bdb51bacc40f3
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-30 15:32:19 +0900

    feat(InputView, OutputView): 입출력 뷰 구현

--- 5-period/java-bridge-main/src/main/java/bridge/view/InputView.java
+++ 5-period/java-bridge-main/src/main/java/bridge/view/InputView.java
@@ -1,5 +1,9 @@
 package bridge.view;
 
+import bridge.error.ErrorMessage;
+import bridge.util.StringParser;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,20 +13,25 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        final String input = readInput();
+        return StringParser.parseToInt(input, ErrorMessage.INVALID_BRIDGE_LENGTH_FORMAT);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        return readInput();
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        return readInput();
+    }
+
+    private String readInput() {
+        return Console.readLine();
     }
 }
