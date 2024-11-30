658d0856dd4f11944afec3a6c7b35bfab202caa3
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-30 15:02:14 +0900

    feat(BridgeGame, BridgeGameTest): 다리 게임 도메인 구현 및 테스트
    테스트한 내용은 다음과 같습니다:
    - 다리길이가 3, 모두 위 다리로 이동 가능 할때 turn 3회 모두 위 다리로 이동하여 게임이 끝난다.
    - 다리길이가 3, 모두 위 다리로 이동 가능 할때 turn 3회 모두 위 다리로 이동하지 않아 게임이 끝나지 않는다.

--- 5-period/java-bridge-main/src/main/java/bridge/domain/BridgeGame.java
+++ 5-period/java-bridge-main/src/main/java/bridge/domain/BridgeGame.java
@@ -1,16 +1,44 @@
 package bridge.domain;
 
+import java.util.List;
+import java.util.Objects;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private static final int INITIAL_TURN = 0;
+
+    private final Bridge bridge;
+    private final UpBridgeLog upBridgeLog;
+    private final DownBridgeLog downBridgeLog;
+    private int turn;
+
+    public BridgeGame(final DownBridgeLog downBridgeLog, final UpBridgeLog upBridgeLog, final Bridge bridge) {
+        this.downBridgeLog = downBridgeLog;
+        this.upBridgeLog = upBridgeLog;
+        this.bridge = bridge;
+        turn = INITIAL_TURN;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(final MoveCommand moveCommand) {
+        final boolean matchedMoveCommand = bridge.isMatchedMoveCommand(turn++, moveCommand);
+        upBridgeLog.updateLog(moveCommand, matchedMoveCommand);
+        downBridgeLog.updateLog(moveCommand, matchedMoveCommand);
+    }
+
+    public List<BridgeLogType> getUpBridgeLogs() {
+        return upBridgeLog.getValues();
+    }
+
+    public List<BridgeLogType> getDownBridgeLogs() {
+        return downBridgeLog.getValues();
     }
 
     /**
@@ -18,6 +46,11 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(final GameCommand gameCommand) {
+        return Objects.equals(gameCommand, GameCommand.RETRY);
+    }
+
+    public boolean isEnd() {
+        return upBridgeLog.countPass() + downBridgeLog.countPass() == turn;
     }
 }
