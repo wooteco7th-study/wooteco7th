823284651ace34542f5d0fe96ac57d0e2af12b39
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-30 14:38:48 +0900

    refactor(MoveCommand, BridgeGenerator): 이동 커멘드 찾는 메서드 추가 및 다리 생성기 수정

--- 5-period/java-bridge-main/src/main/java/bridge/domain/BridgeGenerator.java
+++ 5-period/java-bridge-main/src/main/java/bridge/domain/BridgeGenerator.java
@@ -5,13 +5,19 @@ import java.util.List;
 public class BridgeGenerator {
 
     private BridgeGenerator() {
-        
+
     }
 
     public static Bridge generate(final BridgeLength bridgeLength) {
         final BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
         final BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
         final List<String> bridge = bridgeMaker.makeBridge(bridgeLength.getValue());
-        return new Bridge(bridge);
+        return new Bridge(covertToMoveCommand(bridge));
+    }
+
+    private static List<MoveCommand> covertToMoveCommand(final List<String> bridge) {
+        return bridge.stream()
+                .map(MoveCommand::findByExpression)
+                .toList();
     }
 }
