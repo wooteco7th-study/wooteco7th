b227eacb5f803542d779718240d584b5082e400f
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-30 16:55:34 +0900

    feat(ErrorMessage): 에러 메세지 추가

--- 5-period/java-bridge-main/src/main/java/bridge/error/ErrorMessage.java
+++ 5-period/java-bridge-main/src/main/java/bridge/error/ErrorMessage.java
@@ -4,7 +4,8 @@ public enum ErrorMessage {
 
     INVALID_BRIDGE_LENGTH_FORMAT("다리 길이는 3부터 20 사이의 숫자여야 합니다."),
     INVALID_GAME_COMMAND_FORMAT("R 또는 Q만 입력 가능합니다."),
-    INVALID_MOVE_COMMAND_FORMAT("U 또는 D만 입력 가능합니다.");
+    INVALID_MOVE_COMMAND_FORMAT("U 또는 D만 입력 가능합니다."),
+    INVALID_NOT_FOUND_BRIDGE_LOG("다리 로그를 찾을 수 없습니다.");
 
     private static final String PREFIX = "[ERROR] ";
     private final String message;
