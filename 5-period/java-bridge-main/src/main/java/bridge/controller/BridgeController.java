7b94aaed1cdb7d4b8deb80371ab8088da566685d
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-30 15:58:25 +0900

    feat(BridgeController): 애플리케이션 흐름 제어 클래스 구현

---
+++ 5-period/java-bridge-main/src/main/java/bridge/controller/BridgeController.java
@@ -0,0 +1,85 @@
+package bridge.controller;
+
+import bridge.domain.Bridge;
+import bridge.domain.BridgeGame;
+import bridge.domain.BridgeGenerator;
+import bridge.domain.BridgeLength;
+import bridge.domain.BridgeLogType;
+import bridge.domain.DownBridgeLog;
+import bridge.domain.GameCommand;
+import bridge.domain.MoveCommand;
+import bridge.domain.UpBridgeLog;
+import bridge.util.LoopTemplate;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+import java.util.List;
+
+public class BridgeController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public BridgeController(final InputView inputView, final OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        outputView.printIntro();
+        final BridgeLength bridgeLength = requestBridgeLength();
+        final Bridge bridge = BridgeGenerator.generate(bridgeLength);
+        final BridgeGame bridgeGame = new BridgeGame(new DownBridgeLog(), new UpBridgeLog(), bridge);
+        start(bridgeGame);
+    }
+
+    private void start(final BridgeGame bridgeGame) {
+        do {
+            move(bridgeGame);
+        } while (bridgeGame.retry(requestGameCommand()));
+        responseGameResult(bridgeGame);
+    }
+
+    private void responseGameResult(final BridgeGame bridgeGame) {
+        outputView.printResult(bridgeGame.getUpBridgeLogs(), bridgeGame.getDownBridgeLogs());
+        outputView.printClearAndAttempt(bridgeGame.getGameResult(), bridgeGame.getAttempt());
+    }
+
+    private void move(final BridgeGame bridgeGame) {
+        while (!bridgeGame.isEnd()) {
+            final MoveCommand moveCommand = requestMoveCommand();
+            bridgeGame.move(moveCommand);
+            responseBridgeLog(bridgeGame);
+        }
+
+    }
+
+    private void responseBridgeLog(final BridgeGame bridgeGame) {
+        final List<BridgeLogType> upBridgeLogs = bridgeGame.getUpBridgeLogs();
+        final List<BridgeLogType> downBridgeLogs = bridgeGame.getDownBridgeLogs();
+        outputView.printMap(upBridgeLogs, downBridgeLogs);
+    }
+
+    private BridgeLength requestBridgeLength() {
+        return LoopTemplate.tryCatchLoop(() -> {
+            outputView.printAskBridgeLength();
+            final int bridgeLength = inputView.readBridgeSize();
+            return new BridgeLength(bridgeLength);
+        });
+    }
+
+    private MoveCommand requestMoveCommand() {
+        return LoopTemplate.tryCatchLoop(() -> {
+            outputView.printAskMoveCommand();
+            final String input = inputView.readMoving();
+            return MoveCommand.findByExpression(input);
+        });
+    }
+
+    private GameCommand requestGameCommand() {
+        return LoopTemplate.tryCatchLoop(() -> {
+            outputView.printAskRetry();
+            final String input = inputView.readGameCommand();
+            return GameCommand.findByExpression(input);
+        });
+    }
+}
