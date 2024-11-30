3c2949a7b28b87b931c63995ba8834fea73aa5c7
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-30 14:28:32 +0900

    feat(BridgeLogType, MoveCommand): 다리 로그 타입 및 이동 커멘드 정의

---
+++ 5-period/java-bridge-main/src/main/java/bridge/domain/BridgeLogType.java
@@ -0,0 +1,24 @@
+package bridge.domain;
+
+import java.util.Objects;
+
+public enum BridgeLogType {
+
+    PASS("O"),
+    FAIL("X"),
+    NONE("");
+
+    private final String expression;
+
+    BridgeLogType(final String expression) {
+        this.expression = expression;
+    }
+
+    public String getExpression() {
+        return expression;
+    }
+
+    public static boolean isPassed(final BridgeLogType bridgeLogType) {
+        return Objects.equals(bridgeLogType, BridgeLogType.PASS);
+    }
+}
