28503e1c0f19488f96cf17289935cf0dd88cf221
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-30 14:38:48 +0900

    refactor(MoveCommand, BridgeGenerator): 이동 커멘드 찾는 메서드 추가 및 다리 생성기 수정

--- 5-period/java-bridge-main/src/main/java/bridge/domain/MoveCommand.java
+++ 5-period/java-bridge-main/src/main/java/bridge/domain/MoveCommand.java
@@ -1,5 +1,10 @@
 package bridge.domain;
 
+import bridge.error.CustomIllegalArgumentException;
+import bridge.error.ErrorMessage;
+import java.util.Arrays;
+import java.util.Objects;
+
 public enum MoveCommand {
 
     UP("U"),
@@ -10,6 +15,13 @@ public enum MoveCommand {
 
     MoveCommand(final String expression) {
         this.expression = expression;
+    }
+
+    public static MoveCommand findByExpression(final String expression) {
+        return Arrays.stream(MoveCommand.values())
+                .filter(moveCommand -> Objects.equals(moveCommand.expression, expression))
+                .findAny()
+                .orElseThrow(() -> new CustomIllegalArgumentException(ErrorMessage.INVALID_MOVE_COMMAND_FORMAT));
     }
 
     public String getExpression() {
