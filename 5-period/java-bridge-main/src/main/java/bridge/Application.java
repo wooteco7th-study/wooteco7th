dc76968c2ced3d2b317209c058fb3faa16f02c69
Jiihyun <cwlgusc@gmail.com>
2024-11-30 16:29:54 +0900

    feat: application 의존성 주입

--- 5-period/java-bridge-main/src/main/java/bridge/Application.java
+++ 5-period/java-bridge-main/src/main/java/bridge/Application.java
@@ -1,8 +1,16 @@
 package bridge;
 
+import bridge.controller.GameController;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameController gameController = new GameController(
+                new InputView(),
+                new OutputView()
+        );
+        gameController.run();
     }
 }
