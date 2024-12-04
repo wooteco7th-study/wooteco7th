ce3dbd8cca84a3df378c82007c98e106297fa71a
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-04 22:59:51 +0900

    feat(ErrorMessage): 에러 메세지 추가

--- 3-period/java-subway-path-precourse-main/src/main/java/subway/error/ErrorMessage.java
+++ 3-period/java-subway-path-precourse-main/src/main/java/subway/error/ErrorMessage.java
@@ -4,7 +4,8 @@ public enum ErrorMessage {
 
     INVALID_NOT_FOUND_STATION("역을 찾을 수 없습니다."),
     INVALID_DUPLICATED_STATION("역은 중복될 수 없습니다."),
-    INVALID_COMMAND("올바르지 않은 명령어 입니다.");
+    INVALID_COMMAND("올바르지 않은 명령어 입니다."),
+    INVALID_NOT_LINKED_STATION("역이 서로 연결되어 있지 않습니다.");
 
     private static final String PREFIX = "[ERROR] ";
     private final String message;
