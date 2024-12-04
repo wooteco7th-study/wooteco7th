c8eb7b96466982f33f25fd5d103521358df99526
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-04 23:08:13 +0900

    feat(EdgeGroup): 다익스트라 그래프 가져오는 메서드 구현

--- 3-period/java-subway-path-precourse-main/src/main/java/subway/domain/EdgeGroup.java
+++ 3-period/java-subway-path-precourse-main/src/main/java/subway/domain/EdgeGroup.java
@@ -2,6 +2,9 @@ package subway.domain;
 
 import java.util.Collections;
 import java.util.List;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
 import subway.error.CustomIllegalStateException;
 import subway.error.ErrorMessage;
 
@@ -18,7 +21,7 @@ public class EdgeGroup {
                 .filter(edge -> edge.hasStartAndEnd(start, end))
                 .findAny()
                 .map(Edge::getDistance)
-                .orElseThrow(() -> new CustomIllegalStateException(ErrorMessage.INVALID_NOT_FOUND_STATION));
+                .orElseThrow(() -> new CustomIllegalStateException(ErrorMessage.INVALID_NOT_LINKED_STATION));
     }
 
     public int getTimeByStartAndEnd(final Station start, final Station end) {
@@ -26,7 +29,29 @@ public class EdgeGroup {
                 .filter(edge -> edge.hasStartAndEnd(start, end))
                 .findAny()
                 .map(Edge::getTime)
-                .orElseThrow(() -> new CustomIllegalStateException(ErrorMessage.INVALID_NOT_FOUND_STATION));
+                .orElseThrow(() -> new CustomIllegalStateException(ErrorMessage.INVALID_NOT_LINKED_STATION));
+    }
+
+    public  DijkstraShortestPath getDijkstraShortestTimePath() {
+        WeightedMultigraph<String, DefaultWeightedEdge> graph
+                = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+        for (Edge edge : edges) {
+            graph.addVertex(edge.getStartStationName());
+            graph.addVertex(edge.getEndStationName());
+            graph.setEdgeWeight(graph.addEdge(edge.getStartStationName(), edge.getEndStationName()), edge.getTime());
+        }
+        return new DijkstraShortestPath(graph);
+    }
+
+    public DijkstraShortestPath getDijkstraShortestDistancePath() {
+        WeightedMultigraph<String, DefaultWeightedEdge> graph
+                = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+        for (Edge edge : edges) {
+            graph.addVertex(edge.getStartStationName());
+            graph.addVertex(edge.getEndStationName());
+            graph.setEdgeWeight(graph.addEdge(edge.getStartStationName(), edge.getEndStationName()), edge.getDistance());
+        }
+        return new DijkstraShortestPath(graph);
     }
 
     public List<Edge> getEdges() {
