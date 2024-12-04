e1223440bca122b66df6e59e3339ba17d8281894
Mint <audwl03071@gmail.com>
2024-12-05 00:55:35 +0900

    refactor: 사용하지 않는 테스트 제거

--- 3-period/java-subway-path-precourse-main/src/test/java/subway/JGraphtTest.java
+++ 3-period/java-subway-path-precourse-main/src/test/java/subway/JGraphtTest.java
@@ -1,14 +1,13 @@
 package subway;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class JGraphtTest {
     @Test
@@ -29,33 +28,6 @@ public class JGraphtTest {
 
         GraphPath path = dijkstraShortestPath.getPath("v3", "v1");
         double weight = path.getWeight(); // 가중치 (4)
-
-        assertThat(shortestPath.size()).isEqualTo(3);
-    }
-
-    @Test
-    public void getDijkstraShortestPath2() {
-        WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
-        graph.addVertex("v1");
-        graph.addVertex("v2");
-        graph.addVertex("v3");
-        graph.addVertex("v4");
-        graph.addVertex("v5");
-
-        graph.setEdgeWeight(graph.addEdge("v1", "v2"), 2);
-        graph.setEdgeWeight(graph.addEdge("v2", "v3"), 2);
-        graph.setEdgeWeight(graph.addEdge("v1", "v3"), 100);
-        graph.setEdgeWeight(graph.addEdge("v4", "v5"), 2);
-
-        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
-        List<String> shortestPath = dijkstraShortestPath.getPath("v4", "v5").getVertexList();
-        for (String s : shortestPath) {
-            System.out.println(s); // v3->v2->v1 : 최단 경로
-        }
-
-        GraphPath path = dijkstraShortestPath.getPath("v3", "v1");
-        double weight = path.getWeight(); // 가중치 (4)
-        System.out.println(weight);
 
         assertThat(shortestPath.size()).isEqualTo(3);
     }
