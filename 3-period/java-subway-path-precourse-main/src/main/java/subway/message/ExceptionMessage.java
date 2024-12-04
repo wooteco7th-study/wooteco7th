7e62a5b7af54a6c6eb2d2844c717553e968d44a1
plan11plan <tjtfy7028@gmail.com>
2024-12-04 20:42:32 +0900

    feat: 예외 메시지 추가
    - 값은 null이거나 비어 있을 수 없습니다.
    - 같은 역일 수 없습니다.

--- 3-period/java-subway-path-precourse-main/src/main/java/subway/message/ExceptionMessage.java
+++ 3-period/java-subway-path-precourse-main/src/main/java/subway/message/ExceptionMessage.java
@@ -2,6 +2,8 @@ package subway.message;
 
 
 public enum ExceptionMessage {
+    NULL_OR_EMPTY_MESSAGE("값은 null이거나 비어 있을 수 없습니다."),
+    CANT_BE_SAME_STATION("같은 역일 수 없습니다."),
     DEPARTURE_STATION_CANT_BE_FOUND("찾을 수 없는 출발역 입니다."),
     ARRIVAL_STATION_CANT_BE_FOUND("찾을 수 없는 도착역 입니다."),
     INVALID_FORMAT("잘못된 형식 입니다");
