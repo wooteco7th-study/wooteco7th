8aeafa32db0704b95bfdf383287cd8a3733bed22
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-04 23:30:09 +0900

    refactor(Edge): 유효 검증 메서드 제거

--- 3-period/java-subway-path-precourse-main/src/main/java/subway/domain/Edge.java
+++ 3-period/java-subway-path-precourse-main/src/main/java/subway/domain/Edge.java
@@ -1,9 +1,6 @@
 package subway.domain;
 
-import java.util.List;
 import java.util.Objects;
-import subway.error.ErrorMessage;
-import subway.util.ListValidator;
 
 public class Edge {
 
@@ -14,16 +11,11 @@ public class Edge {
     private final int time;
 
     public Edge(final Line line, final Station start, final Station end, final int distance, final int time) {
-        validate(start, end);
         this.line = line;
         this.start = start;
         this.end = end;
         this.distance = distance;
         this.time = time;
-    }
-
-    private void validate(final Station start, final Station end) {
-        ListValidator.validateDuplicate(List.of(start, end), ErrorMessage.INVALID_DUPLICATED_STATION);
     }
 
     public boolean hasStartAndEnd(final Station start, final Station end) {
