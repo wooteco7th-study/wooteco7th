a1d85e678b013500f42d108d7271fa5bf100126c
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-04 23:29:27 +0900

    feat(Path): 시작점, 도착점 도메인 구현

---
+++ 3-period/java-subway-path-precourse-main/src/main/java/subway/domain/Path.java
@@ -0,0 +1,29 @@
+package subway.domain;
+
+import java.util.List;
+import subway.error.ErrorMessage;
+import subway.util.ListValidator;
+
+public class Path {
+
+    private final Station start;
+    private final Station end;
+
+    public Path(final Station start, final Station end) {
+        validate(start, end);
+        this.start = start;
+        this.end = end;
+    }
+
+    private void validate(final Station start, final Station end) {
+        ListValidator.validateDuplicate(List.of(start, end), ErrorMessage.INVALID_DUPLICATED_STATION);
+    }
+
+    public String getStart() {
+        return start.getName();
+    }
+
+    public String getEnd() {
+        return end.getName();
+    }
+}
