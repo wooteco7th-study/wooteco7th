3804aed08b3415a6eff57ada61abbd0d37bed84a
Jiihyun <cwlgusc@gmail.com>
2024-12-04 15:42:48 +0900

    feat: repo에 line, station 저장

---
+++ 3-period/java-subway-path-precourse-main/src/main/java/subway/controller/SubwayController.java
@@ -0,0 +1,41 @@
+package subway.controller;
+
+import subway.domain.Line;
+import subway.domain.LineRepository;
+import subway.domain.Station;
+import subway.domain.StationRepository;
+import subway.view.InputView;
+import subway.view.OutputView;
+
+import java.util.List;
+
+public class SubwayController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public SubwayController(final InputView inputView, final OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        saveLines();
+        saveStations();
+    }
+
+    private void saveLines() {
+        List<String> lines = List.of("2호선", "3호선", "신분당선");
+        for (String lineName : lines) {
+            Line line = new Line(lineName);
+            LineRepository.addLine(line);
+        }
+    }
+
+    private void saveStations() {
+        List<String> stations = List.of("교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역");
+        for (String stationName : stations) {
+            Station station = new Station(stationName);
+            StationRepository.addStation(station);
+        }
+    }
+}
