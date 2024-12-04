6bcc3848e0e066b1858ceeae28013d4b9cb5e3c4
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-04 23:58:31 +0900

    refactor(LineRepository, StationRepository): 레파지토리 리팩토링

--- 3-period/java-subway-path-precourse-main/src/main/java/subway/domain/StationRepository.java
+++ 3-period/java-subway-path-precourse-main/src/main/java/subway/domain/StationRepository.java
@@ -4,12 +4,25 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import subway.error.CustomIllegalArgumentException;
+import subway.error.ErrorMessage;
 
 public class StationRepository {
     private static final List<Station> stations = new ArrayList<>();
 
+    private StationRepository() {
+
+    }
+
     public static List<Station> stations() {
         return Collections.unmodifiableList(stations);
+    }
+
+    public static Station findByName(final String name) {
+        return stations().stream()
+                .filter(station -> Objects.equals(station.getName(), name))
+                .findAny()
+                .orElseThrow(() -> new CustomIllegalArgumentException(ErrorMessage.INVALID_NOT_FOUND_STATION));
     }
 
     public static void addStation(Station station) {
