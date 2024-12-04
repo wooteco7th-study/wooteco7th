cc86a3fd84c6d9213e36385ed147a7e127f8f0f3
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-04 22:23:58 +0900

    feat(PathCommand): 경로 기준 동작 정의

---
+++ 3-period/java-subway-path-precourse-main/src/main/java/subway/domain/PathCommand.java
@@ -0,0 +1,26 @@
+package subway.domain;
+
+import java.util.Arrays;
+import java.util.Objects;
+import subway.error.CustomIllegalArgumentException;
+import subway.error.ErrorMessage;
+
+public enum PathCommand {
+
+    SHORTEST_DISTANCE("1"),
+    SHORTEST_TIME("2"),
+    BACK("B");
+
+    private String command;
+
+    PathCommand(final String command) {
+        this.command = command;
+    }
+
+    public static PathCommand findByCommand(final String command) {
+        return Arrays.stream(PathCommand.values())
+                .filter(pathCommand -> Objects.equals(pathCommand.command, command))
+                .findAny()
+                .orElseThrow(() -> new CustomIllegalArgumentException(ErrorMessage.INVALID_COMMAND));
+    }
+}
