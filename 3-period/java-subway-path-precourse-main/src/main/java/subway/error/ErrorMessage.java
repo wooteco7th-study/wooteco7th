18c768976cd07601ed172fef98a6793eecd1e302
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-04 23:30:32 +0900

    refactor(ErrorMessage): 에러 메세지 수정

--- 3-period/java-subway-path-precourse-main/src/main/java/subway/error/ErrorMessage.java
+++ 3-period/java-subway-path-precourse-main/src/main/java/subway/error/ErrorMessage.java
@@ -3,7 +3,7 @@ package subway.error;
 public enum ErrorMessage {
 
     INVALID_NOT_FOUND_STATION("역을 찾을 수 없습니다."),
-    INVALID_DUPLICATED_STATION("역은 중복될 수 없습니다."),
+    INVALID_DUPLICATED_STATION("출발역과 도착역이 동일합니다."),
     INVALID_COMMAND("올바르지 않은 명령어 입니다."),
     INVALID_NOT_LINKED_STATION("역이 서로 연결되어 있지 않습니다.");
 
