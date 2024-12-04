ff75a3f2e4a8168c0198227959e05737a046c731
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-04 21:48:13 +0900

    feat(EdgeInfo): 간선 정보 정의

---
+++ 3-period/java-subway-path-precourse-main/src/main/java/subway/domain/EdgeInfo.java
@@ -0,0 +1,57 @@
+package subway.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public enum EdgeInfo {
+
+    INFO_1(LineType.TWO_LINE, StationType.GYODAE, StationType.GANGNAM, 2, 3),
+    INFO_2(LineType.TWO_LINE, StationType.GANGNAM, StationType.YEOKSAM, 2, 3),
+    INFO_3(LineType.TREE_LINE, StationType.GYODAE, StationType.NAMBU_TERMINAL, 3, 2),
+    INFO_4(LineType.TREE_LINE, StationType.NAMBU_TERMINAL, StationType.YANGJAE, 6, 5),
+    INFO_5(LineType.TREE_LINE, StationType.NAMBU_TERMINAL, StationType.MAEBONG, 1, 1),
+    INFO_6(LineType.SHINBUNDANG_LINE, StationType.GANGNAM, StationType.YANGJAE, 2, 8),
+    INFO_7(LineType.SHINBUNDANG_LINE, StationType.YANGJAE, StationType.YANGJAE_CITIZEN_FOREST, 10, 3);
+
+
+    private LineType lineType;
+    private StationType start;
+    private StationType end;
+    private int distance;
+    private int time;
+
+    EdgeInfo(final LineType lineType, final StationType start, final StationType end, final int distance,
+             final int time) {
+        this.lineType = lineType;
+        this.start = start;
+        this.end = end;
+        this.distance = distance;
+        this.time = time;
+    }
+
+    public static List<EdgeInfo> findAll() {
+        return Arrays.stream(EdgeInfo.values())
+                .collect(Collectors.toList());
+    }
+
+    public LineType getLineType() {
+        return lineType;
+    }
+
+    public StationType getStart() {
+        return start;
+    }
+
+    public StationType getEnd() {
+        return end;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public int getTime() {
+        return time;
+    }
+}
