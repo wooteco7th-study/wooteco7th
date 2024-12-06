35fb7aff98cfd31d787bb2a841d623afaf345447
plan11plan <tjtfy7028@gmail.com>
2024-12-07 00:17:16 +0900

    style: 오탈자 정정

--- 4-period/java-pairmatching-precourse-main/src/main/java/pairmatching/constant/PairMatchingRule.java
+++ 4-period/java-pairmatching-precourse-main/src/main/java/pairmatching/constant/PairMatchingRule.java
@@ -4,8 +4,8 @@ package pairmatching.constant;
 - 최대 다시 섞는 횟수는 3이다.
 
  */
-public class pairMatchingRule {
-    private pairMatchingRule() {
+public class PairMatchingRule {
+    private PairMatchingRule() {
     }
 
     public final static int TRY_SHUFFLE_COUNT_MAX = 3;
