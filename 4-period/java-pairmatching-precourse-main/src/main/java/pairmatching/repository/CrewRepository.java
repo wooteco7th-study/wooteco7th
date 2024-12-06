cb9eac00c9f6c0ef706482aba27f3b3f1ceebfea
plan11plan <tjtfy7028@gmail.com>
2024-12-07 00:16:48 +0900

    feat: CrewRepository 코스로 크루찾기 기능구현

--- 4-period/java-pairmatching-precourse-main/src/main/java/pairmatching/repository/CrewRepository.java
+++ 4-period/java-pairmatching-precourse-main/src/main/java/pairmatching/repository/CrewRepository.java
@@ -1,8 +1,12 @@
 package pairmatching.repository;
 
+import static pairmatching.domain.vo.Course.BACKEND;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import pairmatching.domain.Crew;
+import pairmatching.domain.vo.Course;
 
 public class CrewRepository {
     private final List<Crew> crews = new ArrayList<>();
@@ -13,6 +17,17 @@ public class CrewRepository {
         List<Crew> frontend = fileLoader.initializeFrontend();
         backend.forEach(crews::add);
         frontend.forEach(crews::add);
+    }
+
+    public List<String> findAllByCourse(final Course course) {
+        if (course.equals(BACKEND)) {
+            return crews.stream()
+                    .filter(Crew::isBackend)
+                    .map(crew -> crew.getName()).collect(Collectors.toList());
+        }
+        return crews.stream()
+                .filter(Crew::isFrontend)
+                .map(crew -> crew.getName()).collect(Collectors.toList());
     }
 
 }
