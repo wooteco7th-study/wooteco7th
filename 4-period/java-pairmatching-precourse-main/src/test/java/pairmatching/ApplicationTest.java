04d27efe749216db53abdc8173d5e2b07322d543
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-07 00:29:53 +0900

    test(ApplicationTest): 애플리케이션 테스트
    테스트한 내용은 다음과 같습니다:
    - 3회 초과 예외 테스트
    - 홀수 인원 매칭 테스트
    - 이미 존재하는 페어 매칭 테스트

--- 4-period/java-pairmatching-precourse-main/src/test/java/pairmatching/ApplicationTest.java
+++ 4-period/java-pairmatching-precourse-main/src/test/java/pairmatching/ApplicationTest.java
@@ -6,8 +6,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import pairmatching.Application;
+import pairmatching.error.ErrorMessage;
 
 class ApplicationTest extends NsTest {
 
@@ -31,6 +32,48 @@ class ApplicationTest extends NsTest {
                 runException("1", "백엔드, 레벨1, 오징어게임");
                 assertThat(output()).contains(ERROR_MESSAGE);
             }
+        );
+    }
+
+    @Test
+    @DisplayName("3회 초과 예외 테스트")
+    void applicationTest1() throws Exception {
+        assertShuffleTest(
+                () -> {
+                    run("1", "백엔드, 레벨1, 자동차경주", "1", "백엔드, 레벨1, 로또", "Q");
+                    assertThat(output()).contains("태웅 : 백호", "치수 : 태섭");
+                    assertThat(output()).contains(ErrorMessage.INVALID_FAIL_MATCH.getMessage());
+                },
+                Arrays.asList("태웅", "백호", "치수", "태섭")
+        );
+    }
+
+    @Test
+    @DisplayName("홀수 인원 매칭 테스트")
+    void applicationTest2() throws Exception {
+        assertShuffleTest(
+                () -> {
+                    run("1", "백엔드, 레벨1, 자동차경주", "Q");
+                    assertThat(output()).contains("태웅 : 백호", "치수 : 태섭 : 형균");
+                },
+                Arrays.asList("태웅", "백호", "치수", "태섭", "형균")
+        );
+    }
+
+    @Test
+    @DisplayName("이미 존재하는 페어 매칭 테스트")
+    void applicationTest3() throws Exception {
+        assertShuffleTest(
+                () -> {
+                    run("1", "백엔드, 레벨1, 자동차경주", "1", "백엔드, 레벨1, 자동차경주", "네", "Q");
+                    assertThat(output()).contains("태웅 : 백호", "치수 : 태섭 : 형균");
+                    assertThat(output()).contains("매칭 정보가 있습니다. 다시 매칭하시겠습니까?\n"
+                            + "네 | 아니오");
+                    assertThat(output()).contains("치수 : 형균 : 백호");
+
+                },
+                Arrays.asList("태웅", "백호", "치수", "태섭", "형균"),
+                Arrays.asList("치수", "형균", "백호")
         );
     }
 
