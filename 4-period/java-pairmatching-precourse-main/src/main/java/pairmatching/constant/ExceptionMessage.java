24afda51d9b986a05673add18204213765565719
plan11plan <tjtfy7028@gmail.com>
2024-12-07 00:16:57 +0900

    feat: 예외 메시지 추가

--- 4-period/java-pairmatching-precourse-main/src/main/java/pairmatching/constant/ExceptionMessage.java
+++ 4-period/java-pairmatching-precourse-main/src/main/java/pairmatching/constant/ExceptionMessage.java
@@ -4,6 +4,7 @@ public enum ExceptionMessage {
     CANT_NULL_OR_EMPTY("값은 null이거나 비어 있을 수 없습니다."),
     INVALID_INPUT_FORM("유효하지 않은 형식 입니다."),
     NO_MATCHING_HISTORY("매칭 이력이 없습니다."),
+    OVER_THREE_SHUFFLE("[ERROR] 페어 매칭이 불가능합니다."),
     HAS_MATCH_INFO_BUT_TRY_REMATCH("매칭 정보가 있습니다. 다시 매칭하시겠습니까?");
 
 
