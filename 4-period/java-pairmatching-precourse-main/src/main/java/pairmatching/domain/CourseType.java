944c5ba52f7d5a5522d76bd212c1f7cb654de1ac
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-06 20:31:57 +0900

    refactor: 과정 리팩토링

--- 4-period/java-pairmatching-precourse-main/src/main/java/pairmatching/domain/CourseType.java
+++ 4-period/java-pairmatching-precourse-main/src/main/java/pairmatching/domain/CourseType.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 import pairmatching.error.AppException;
 import pairmatching.error.ErrorMessage;
 
-public enum Course {
+public enum CourseType {
 
     BACK_END("백엔드"),
     FRONT_END("프론트엔드");
@@ -13,12 +13,12 @@ public enum Course {
 
     private final String name;
 
-    Course(final String name) {
+    CourseType(final String name) {
         this.name = name;
     }
 
-    public static Course findByName(final String name) {
-        return Arrays.stream(Course.values())
+    public static CourseType findByName(final String name) {
+        return Arrays.stream(CourseType.values())
                 .filter(course -> Objects.equals(course.name, name))
                 .findAny()
                 .orElseThrow(() -> new AppException(ErrorMessage.INVALID_COURSE));
