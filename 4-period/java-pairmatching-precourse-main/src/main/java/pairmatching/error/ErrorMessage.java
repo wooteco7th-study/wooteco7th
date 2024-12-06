e5b8cd78e1e8511d7267987936a0ff03b800a4b5
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-06 23:40:04 +0900

    feat(ErrorMessage): 에러 메세지 추가

--- 4-period/java-pairmatching-precourse-main/src/main/java/pairmatching/error/ErrorMessage.java
+++ 4-period/java-pairmatching-precourse-main/src/main/java/pairmatching/error/ErrorMessage.java
@@ -6,6 +6,7 @@ public enum ErrorMessage {
     INVALID_COURSE("유효하지 않은 과정 입니다."),
     INVALID_MISSION("유효하지 않은 미션 입니다."),
     INVALID_COMMAND("유효하지 않은 기능 입니다."),
+    INVALID_INPUT_FORMAT("유효하지 않은 입력 입니다."),
     INVALID_FAIL_MATCH("매칭 시도가 3회를 초과하여 매칭에 실패하였습니다."),
     INVALID_NOT_FOUND_PAIR_RESULT("매칭 이력이 없습니다."),
     INVALID_FAIL_FILE_LOAD("파일을 읽는데 실패하였습니다.");
