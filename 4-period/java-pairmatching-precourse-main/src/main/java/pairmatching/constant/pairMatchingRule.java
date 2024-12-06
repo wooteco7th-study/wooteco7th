d7202e7f5749ae13f7aceff3353c5ca6b3d275ff
plan11plan <tjtfy7028@gmail.com>
2024-12-06 20:16:07 +0900

    feat: 애플리케이션 규칙 상수 정의

---
+++ 4-period/java-pairmatching-precourse-main/src/main/java/pairmatching/constant/pairMatchingRule.java
@@ -0,0 +1,13 @@
+package pairmatching.constant;
+
+/*
+- 최대 다시 섞는 횟수는 3이다.
+
+ */
+public class pairMatchingRule {
+    private pairMatchingRule() {
+    }
+
+    public final static int TRY_SHUFFLE_COUNT_MAX = 3;
+
+}
