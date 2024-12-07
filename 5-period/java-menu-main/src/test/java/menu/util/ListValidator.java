d1b5b7ad9c41cf72b9a72e4819fefde49dd88676
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-07 20:42:44 +0900

    feat(ListValidator): 리스트 중복 검증 메서드 추가

--- 5-period/java-menu-main/src/test/java/menu/util/ListValidator.java
+++ 5-period/java-menu-main/src/test/java/menu/util/ListValidator.java
@@ -11,8 +11,22 @@ public class ListValidator {
     }
 
 
-    public static <T> void validateSize(final List<T> values, final int size, final ErrorMessage errorMessage) {
-        if (values.size() != size) {
+    public static <T> void validateDuplicate(final List<T> values, final ErrorMessage errorMessage) {
+        if (isDuplicated(values)) {
+            throw new AppException(errorMessage);
+        }
+    }
+
+
+    private static <T> boolean isDuplicated(final List<T> values) {
+        return values.stream()
+                .distinct()
+                .count() != values.size();
+    }
+
+    public static <T> void validateSize(final List<T> values, final int min, final int max,
+                                        final ErrorMessage errorMessage) {
+        if (values.size() < min || values.size() > max) {
             throw new AppException(errorMessage);
         }
     }
