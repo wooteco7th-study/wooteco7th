7721cf36fca98c847eb26ca9ba11e326a9289cd1
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-07 20:26:02 +0900

    feat(ErrorMessage, MenuType): 에러 메세지 추가 및 메뉴 타입 정의

--- 5-period/java-menu-main/src/test/java/menu/error/ErrorMessage.java
+++ 5-period/java-menu-main/src/test/java/menu/error/ErrorMessage.java
@@ -4,7 +4,8 @@ public enum ErrorMessage {
 
     INVALID_COACHES_SIZE("코치는 최소 2명 이상 입력해야 합니다."),
     INVALID_COACH_NAME("코치의 이름은 최소 2글자, 최대 4글자이어야 합니다."),
-    INVALID_HATE_MENU("못 먹는 메뉴는 최대 2개까지만 가능합니다.");
+    INVALID_HATE_MENU("못 먹는 메뉴는 최대 2개까지만 가능합니다."),
+    INVALID_NOT_FOUND_MENU_TYPE("메뉴 타입을 찾을 수 없습니다.");
 
     private static final String PREFIX = "[ERROR] ";
     private final String message;
