641890af250d3d55971dd6752a9dbedc7106fed0
plan11plan <tjtfy7028@gmail.com>
2024-12-07 20:11:13 +0900

    feat: 코치 사이즈 수 상수 규칙 추가

--- 5-period/java-menu-main/src/main/java/menu/constant/ApplicationRule.java
+++ 5-period/java-menu-main/src/main/java/menu/constant/ApplicationRule.java
@@ -1,12 +1,19 @@
 package menu.constant;
 
 public class ApplicationRule {
-    private final int COACH_NAME_MAX = 4;
-    private final int COACH_NAME_MIN = 2;
+    private ApplicationRule() {
+    }
 
-    private final int BAN_MENU_MIN = 0;
-    private final int BAN_MENU_MAX = 2;
+    public static final int COACH_NAME_MAX = 4;
+    public static final int COACH_NAME_MIN = 2;
 
-    private final int ONE_WEEK_SAME_CATEGORY_MAX = 2;
+    public static final int COACH_SIZE_MAX = 5;
+    public static final int COACH_SIZE_MIN = 2;
+
+
+    public static final int BAN_MENU_MIN = 0;
+    public static final int BAN_MENU_MAX = 2;
+
+    public static final int ONE_WEEK_SAME_CATEGORY_MAX = 2;
 
 }
