d1afe0bc9f64b976842995d7ccf18b400feeecfb
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-07 23:44:58 +0900

    feat(ErrorMessage): 코치 5명 넘을때 예외 메세지 추가

--- 5-period/java-menu-main/src/main/java/menu/error/ErrorMessage.java
+++ 5-period/java-menu-main/src/main/java/menu/error/ErrorMessage.java
@@ -2,7 +2,8 @@ package menu.error;
 
 public enum ErrorMessage {
 
-    INVALID_COACHES_SIZE("코치는 최소 2명 이상 입력해야 합니다."),
+    INVALID_COACHES_MIN_SIZE("코치는 최소 2명 이상 입력해야 합니다."),
+    INVALID_COACHES_MAX_SIZE("코치는 최대 5명 까지만 입력 가능 합니다."),
     INVALID_COACH_NAME("코치의 이름은 최소 2글자, 최대 4글자이어야 합니다."),
     INVALID_HATE_MENU("못 먹는 메뉴는 최대 2개까지만 가능합니다."),
     INVALID_NOT_FOUND_MENU("메뉴가 존재하지 않습니다."),
