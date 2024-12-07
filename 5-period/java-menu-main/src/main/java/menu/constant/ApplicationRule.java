14e403cb66ea732698f097a65f87689c33c0169a
plan11plan <tjtfy7028@gmail.com>
2024-12-07 20:01:01 +0900

    feat: 애플리케이션 규칙 상수 정의

---
+++ 5-period/java-menu-main/src/main/java/menu/constant/ApplicationRule.java
@@ -0,0 +1,12 @@
+package menu.constant;
+
+public class ApplicationRule {
+    private final int COACH_NAME_MAX = 4;
+    private final int COACH_NAME_MIN = 2;
+
+    private final int BAN_MENU_MIN = 0;
+    private final int BAN_MENU_MAX = 2;
+
+    private final int ONE_WEEK_SAME_CATEGORY_MAX = 2;
+
+}
