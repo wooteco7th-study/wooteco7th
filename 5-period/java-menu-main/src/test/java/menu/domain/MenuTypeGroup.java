cbb6b8fed6ac096768c1c5f4050dac92765da4e0
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-07 21:14:07 +0900

    feat(MenuTypeGroup): 메뉴 타입 그룹 도메인 구현

---
+++ 5-period/java-menu-main/src/test/java/menu/domain/MenuTypeGroup.java
@@ -0,0 +1,30 @@
+package menu.domain;
+
+import java.util.Collections;
+import java.util.List;
+
+public class MenuTypeGroup {
+
+    private static final int MIN = 1;
+    private static final int MAX = 5;
+    private final List<MenuType> menuTypes;
+
+    public MenuTypeGroup(final List<MenuType> menuTypes) {
+        this.menuTypes = menuTypes;
+    }
+
+    public MenuType updateMenuTypes() {
+        final MenuType menuType = RandomMenuTypeGenerator.generate(MIN, MAX);
+        if (Collections.frequency(menuTypes, menuType) < 2) {
+            menuTypes.add(menuType);
+            return menuType;
+        }
+        return updateMenuTypes();
+    }
+
+    public List<String> getMenuTypes() {
+        return menuTypes.stream()
+                .map(MenuType::getName)
+                .toList();
+    }
+}
