e3afe104907a17be7e31a5e473d25c8a26a79f24
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-07 23:55:34 +0900

    test(CoachNameGroupTest): 코치 이름 그룹 테스트 수정

--- 5-period/java-menu-main/src/test/java/menu/domain/CoachNameGroupTest.java
+++ 5-period/java-menu-main/src/test/java/menu/domain/CoachNameGroupTest.java
@@ -3,29 +3,31 @@ package menu.domain;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import java.util.List;
-import java.util.stream.Stream;
 import menu.error.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 
 class CoachNameGroupTest {
 
-    @ParameterizedTest
-    @MethodSource("invalidSize")
-    @DisplayName("코치가 2~5명을 벗어나 예외가 발생한다.")
-    void validateSize(final List<CoachName> coachNames) throws Exception {
+    @Test
+    @DisplayName("코치가 2명 미만 이므로 예외가 발생한다.")
+    void validateMinSizeTest() throws Exception {
+        //given
+        final List<CoachName> coachNames = List.of(new CoachName("수달"));
         //should
         assertThatIllegalArgumentException().isThrownBy(() -> new CoachNameGroup(coachNames))
-                .withMessageContaining(ErrorMessage.INVALID_COACHES_SIZE.getMessage());
+                .withMessageContaining(ErrorMessage.INVALID_COACHES_MIN_SIZE.getMessage());
     }
 
-
-    private static Stream<List<CoachName>> invalidSize() {
-        return Stream.of(
-                List.of(new CoachName("수달")),
-                List.of(new CoachName("수달"), new CoachName("밍트"), new CoachName("러키"), new CoachName("람쥐"), new CoachName("로케"), new CoachName("희용")
-        ));
+    @Test
+    @DisplayName("코치가 5명을 초과하여 예외가 발생한다.")
+    void validateMaxSizeTest() throws Exception {
+        //given
+        final List<CoachName> coachNames = List.of(new CoachName("수달"), new CoachName("밍트"), new CoachName("러키"),
+                new CoachName("람쥐"), new CoachName("로케"), new CoachName("희용"));
+        //should
+        assertThatIllegalArgumentException().isThrownBy(() -> new CoachNameGroup(coachNames))
+                .withMessageContaining(ErrorMessage.INVALID_COACHES_MAX_SIZE.getMessage());
     }
 
 }
