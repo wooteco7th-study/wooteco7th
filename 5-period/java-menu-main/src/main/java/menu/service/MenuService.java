b195fdf784b0a925510e6480f4dbda50a83ee3fd
plan11plan <tjtfy7028@gmail.com>
2024-12-07 22:18:29 +0900

    feat: 테스트 통과 확인

--- 5-period/java-menu-main/src/main/java/menu/service/MenuService.java
+++ 5-period/java-menu-main/src/main/java/menu/service/MenuService.java
@@ -1,5 +1,9 @@
 package menu.service;
 
+import java.util.List;
+import menu.domain.Coach;
+import menu.domain.vo.Menu;
+import menu.dto.RecommendDto;
 import menu.repository.CoachRepository;
 
 public class MenuService {
@@ -7,5 +11,17 @@ public class MenuService {
 
     public MenuService(final CoachRepository coachRepository) {
         this.coachRepository = coachRepository;
+    }
+
+    public List<Coach> createCoach(final List<Coach> coaches) {
+        return null;
+    }
+
+    public List<Coach> createCoachNoMenu(final Coach coach, final List<Menu> menus) {
+        return null;
+    }
+
+    public RecommendDto getRecommendMenus(final List<Coach> coaches) {
+        return null;
     }
 }
