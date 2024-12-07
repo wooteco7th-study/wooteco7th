cd58d641bbdda5c816bb7a062a167e4d48e4deed
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-07 20:11:49 +0900

    feat(AppException, ErrorMessage): 커스텀 예외 및 에러 메세지 정의

---
+++ 5-period/java-menu-main/src/test/java/menu/error/AppException.java
@@ -0,0 +1,7 @@
+package menu.error;
+
+public class AppException extends IllegalArgumentException {
+    public AppException(final ErrorMessage errorMessage) {
+        super(errorMessage.getMessage());
+    }
+}
