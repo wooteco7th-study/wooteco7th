dada9750723ac3d62e042363fa9dc89359b3d96b
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-09 20:55:14 +0900

    feat(WeekdayWorkerGroup, WeekendWorkerGroup): 평일 휴일 비상 근무자 도메인 구현

---
+++ 6-period/java-oncall-6-main/src/main/java/oncall/domain/WeekendWorkerGroup.java
@@ -0,0 +1,33 @@
+package oncall.domain;
+
+import java.util.ArrayDeque;
+import java.util.List;
+import oncall.error.ErrorMessage;
+import oncall.util.ListValidator;
+
+public class WeekendWorkerGroup {
+
+    private final ArrayDeque<String> workers;
+
+    public WeekendWorkerGroup(final List<String> workers) {
+        validate(workers);
+        this.workers = new ArrayDeque<>(workers);
+    }
+
+    private void validate(final List<String> workers) {
+        ListValidator.validateDuplicate(workers, ErrorMessage.INVALID_WORKER);
+    }
+
+    public String getNextWorker() {
+        final String worker = workers.pollFirst();
+        workers.offerLast(worker);
+        return worker;
+    }
+
+    public String getChangedWorker(final String worker) {
+        final String nextWorker = workers.pollFirst();
+        workers.offerLast(nextWorker);
+        workers.offerFirst(worker);
+        return nextWorker;
+    }
+}
