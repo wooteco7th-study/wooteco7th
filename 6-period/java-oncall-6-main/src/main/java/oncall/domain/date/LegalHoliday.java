aa3be1d3d3dfd5e5b74e6d78c33e12a7f3a0c07d
Mint <audwl03071@gmail.com>
2024-12-09 22:43:33 +0900

    refactor: 포맷팅

--- 6-period/java-oncall-6-main/src/main/java/oncall/domain/date/LegalHoliday.java
+++ 6-period/java-oncall-6-main/src/main/java/oncall/domain/date/LegalHoliday.java
@@ -6,8 +6,7 @@ public enum LegalHoliday {
 
     신정(1, 1), 삼일절(3, 1), 어린이날(5, 5),
     현충일(6, 6), 광복절(8, 15), 개천절(10, 3),
-    한글날(10, 9), 성탄절(12, 25),
-    ;
+    한글날(10, 9), 성탄절(12, 25);
 
     private final int month;
     private final int day;
--- 6-period/java-oncall-6-main/src/main/java/oncall/exception/ErrorMessage.java
+++ 6-period/java-oncall-6-main/src/main/java/oncall/exception/ErrorMessage.java
@@ -2,31 +2,8 @@ package oncall.exception;
 
 public enum ErrorMessage {
 
-    // - 월
-    //    - [ ]  문자가 아닌 경우
-    //    - [ ]  1~12 사이의 숫자가 아닌 경우
-    //- 요일
-    //    - [ ]  월~금 사이의 문자가 아닌 경우
-    //    - [ ]  해당 달의 시작 요일이 아닌 경우
     INVALID_INPUT("유효하지 않은 입력 값입니다. 다시 입력해 주세요."),
-    CANNOT_CALCULATE_DAYTYPE("적절한 DayType을 반환할 수 없습니다."),
-
-    // - 닉네임
-    //    - [ ]  빈 값이거나 5자 초과일 경우
-    //- 근무자수
-    //    - [ ]  최소 5명~최대 35명 범위가 아닐 경우
-    //- 평일 비상 근무 순번
-    //    - [ ]  중복 이름이 존재하는 경우
-    //- 휴일 비상 근무 순번
-    //    - [ ]  중복 이름이 존재하는 경우
-    //- [ ]  평일 순번과 휴일 순번에 근무자가 각각 한번씩 배정되지 않는 경우
-    //    - [ ]  평일 비상 근무와 휴일 비상 근무의 크기가 다른 경우
-//    INVALID_NICKNAME("유효한 닉네임이 아닙니다."),
-//    INVALID_WORKER_NUMBER("유효한 근무자 수 범위가 아닙니다."),
-//    INVALID_DUPLICATED_NAME("순번 내에 중복 이름이 존재합니다."),
-//    INVALID_TURN("근무자가 평일과 주말에 올바르게 배치되지 않았습니다."),
-
-    ;
+    CANNOT_CALCULATE_DAYTYPE("적절한 DayType을 반환할 수 없습니다.");
 
     private final String message;
 
