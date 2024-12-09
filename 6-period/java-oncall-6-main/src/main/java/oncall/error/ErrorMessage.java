72eb76a2627510420892c0b6c9c0f3a111671c3f
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-09 22:00:41 +0900

    feat(ErrorMessage): 에러 메세지 추가

--- 6-period/java-oncall-6-main/src/main/java/oncall/error/ErrorMessage.java
+++ 6-period/java-oncall-6-main/src/main/java/oncall/error/ErrorMessage.java
@@ -4,13 +4,15 @@ public enum ErrorMessage {
 
     INVALID_MONTH("유효하지 않은 월 입니다."),
     INVALID_WEEK("유효하지 않은 요일 입니다."),
-    INVALID_WORKER("유효하지 않은 입력 값입니다. 다시 입력해 주세요.");
+    INVALID_WORKER("유효하지 않은 입력 값입니다."),
+    INVALID_WORK_DAY("유효하지 않은 날짜 입니다.");
 
     private static final String PREFIX = "[ERROR] ";
+    private static final String SUFFIX = " 다시 입력해 주세요.";
     private final String message;
 
     ErrorMessage(final String message) {
-        this.message = PREFIX + message;
+        this.message = PREFIX + message + SUFFIX;
     }
 
     public String getMessage() {
