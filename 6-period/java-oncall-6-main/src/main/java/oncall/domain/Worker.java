79329a5f10a71b6b181e3a280951fb0b8a6186ba
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-09 22:01:27 +0900

    feat(Worker): 이름 및 일 추가

--- 6-period/java-oncall-6-main/src/main/java/oncall/domain/Worker.java
+++ 6-period/java-oncall-6-main/src/main/java/oncall/domain/Worker.java
@@ -2,13 +2,17 @@ package oncall.domain;
 
 public class Worker {
 
+    private final String name;
     private final Month month;
     private final DayOfWeek dayOfWeek;
+    private final int dayOfMonth;
     private final boolean isHoliday;
 
-    public Worker(final Month month, final DayOfWeek dayOfWeek, final boolean isHoliday) {
+    public Worker(final String name, final Month month, final DayOfWeek dayOfWeek, final int dayOfMonth, final boolean isHoliday) {
+        this.name = name;
         this.month = month;
         this.dayOfWeek = dayOfWeek;
+        this.dayOfMonth = dayOfMonth;
         this.isHoliday = isHoliday;
     }
 
@@ -20,7 +24,15 @@ public class Worker {
         return dayOfWeek;
     }
 
+    public int getDayOfMonth() {
+        return dayOfMonth;
+    }
+
     public boolean isHoliday() {
         return isHoliday;
+    }
+
+    public String getName() {
+        return name;
     }
 }
