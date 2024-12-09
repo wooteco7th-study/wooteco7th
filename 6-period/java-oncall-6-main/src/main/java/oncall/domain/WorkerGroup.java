3729a4e951e72346e3478058614f2ed20e4198ed
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-10 00:04:51 +0900

    refactor: WorkerGroup 추상화

--- 6-period/java-oncall-6-main/src/main/java/oncall/domain/WorkerGroup.java
+++ 6-period/java-oncall-6-main/src/main/java/oncall/domain/WorkerGroup.java
@@ -1,28 +1,47 @@
 package oncall.domain;
 
-public class WorkerGroup {
+import java.util.ArrayDeque;
+import java.util.List;
+import oncall.error.ErrorMessage;
+import oncall.util.ListValidator;
+import oncall.util.NumberValidator;
 
-    private final WeekdayWorkerGroup weekdayWorkerGroup;
-    private final WeekendWorkerGroup weekendWorkerGroup;
+public abstract class WorkerGroup {
 
-    public WorkerGroup(final WeekdayWorkerGroup weekdayWorkerGroup, final WeekendWorkerGroup weekendWorkerGroup) {
-        this.weekdayWorkerGroup = weekdayWorkerGroup;
-        this.weekendWorkerGroup = weekendWorkerGroup;
+    private static final int NAME_MIN = 0;
+    private static final int NAME_MAX = 5;
+    private static final int MIN = 5;
+    private static final int MAX = 35;
+
+    private final ArrayDeque<String> workers;
+
+    protected WorkerGroup(final List<String> workers) {
+        validate(workers);
+        this.workers = new ArrayDeque<>(workers);
     }
 
-    public String getWeekDayNextWorker() {
-        return weekdayWorkerGroup.getNextWorker();
+    public String getNextWorker() {
+        final String worker = workers.pollFirst();
+        workers.offerLast(worker);
+        return worker;
     }
 
-    public String getWeekDayChangedWorker(final String worker) {
-        return weekdayWorkerGroup.getChangedWorker(worker);
+    public String getChangedWorker(final String worker) {
+        final String nextWorker = workers.pollFirst();
+        workers.offerLast(nextWorker);
+        workers.offerFirst(worker);
+        return nextWorker;
     }
 
-    public String getWeekendNextWorker() {
-        return weekendWorkerGroup.getNextWorker();
+    private void validate(final List<String> workers) {
+        ListValidator.validateDuplicate(workers, ErrorMessage.INVALID_INPUT);
+        NumberValidator.validateRange(workers.size(), MIN, MAX, ErrorMessage.INVALID_INPUT);
+        validateLength(workers);
     }
 
-    public String getWeekendChangedWorker(final String worker) {
-        return weekendWorkerGroup.getChangedWorker(worker);
+    private void validateLength(final List<String> workers) {
+        for (String worker : workers) {
+            NumberValidator.validateRange(worker.length(), NAME_MIN, NAME_MAX, ErrorMessage.INVALID_INPUT);
+        }
     }
 }
