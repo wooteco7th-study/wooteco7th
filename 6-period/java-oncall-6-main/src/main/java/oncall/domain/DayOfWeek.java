b02bc3b9cfec43272db7bae006625b58764a5b6d
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-09 22:00:21 +0900

    feat(DayOfWeek): 휴일 여부 추가

--- 6-period/java-oncall-6-main/src/main/java/oncall/domain/DayOfWeek.java
+++ 6-period/java-oncall-6-main/src/main/java/oncall/domain/DayOfWeek.java
@@ -6,21 +6,23 @@ import oncall.error.AppException;
 import oncall.error.ErrorMessage;
 
 public enum DayOfWeek {
-    SUNDAY("일", 0),
-    MONDAY("월", 1),
-    TUESDAY("화", 2),
-    WEDNESDAY("수", 3),
-    THURSDAY("목", 4),
-    FRIDAY("금", 5),
-    SATURDAY("토", 6);
+    SUNDAY("일", 0, true),
+    MONDAY("월", 1, false),
+    TUESDAY("화", 2, false),
+    WEDNESDAY("수", 3,false),
+    THURSDAY("목", 4, false),
+    FRIDAY("금", 5, false),
+    SATURDAY("토", 6, true);
 
 
     private final String name;
     private final int index;
+    private final boolean isWeekend;
 
-    DayOfWeek(final String name, final int index) {
+    DayOfWeek(final String name, final int index, final boolean isWeekend) {
         this.name = name;
         this.index = index;
+        this.isWeekend = isWeekend;
     }
 
     public static DayOfWeek findByName(final String name) {
@@ -40,4 +42,7 @@ public enum DayOfWeek {
                 .orElseThrow(() -> new AppException(ErrorMessage.INVALID_WEEK));
     }
 
+    public boolean isWeekend() {
+        return isWeekend;
+    }
 }
