3d75c4e1deafb173c67288aeac57b03bd6ef5d70
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-09 20:04:08 +0900

    feat: 유틸 클래스 구현

---
+++ 6-period/java-oncall-6-main/src/test/java/oncall/util/ListValidator.java
@@ -0,0 +1,30 @@
+package oncall.util;
+
+import java.util.List;
+import oncall.error.AppException;
+import oncall.error.ErrorMessage;
+
+public class ListValidator {
+
+        private ListValidator() {
+
+            }
+
+            public static <T> void validateDuplicate(final List<T> values, final ErrorMessage errorMessage) {
+                if (isDuplicated(values)) {
+                    throw new AppException(errorMessage);
+                }
+            }
+
+
+            private static <T> boolean isDuplicated(final List<T> values) {
+                return values.stream()
+                        .distinct()
+                        .count() != values.size();
+            }
+
+            @FunctionalInterface
+            public interface ValidateFunction<T extends Number> {
+                void validate(final T value);
+            }
+}
