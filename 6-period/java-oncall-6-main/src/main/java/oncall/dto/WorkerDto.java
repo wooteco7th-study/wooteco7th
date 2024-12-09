2f27846c11c82a8fcd3ea5af44249a8d6f3bbf77
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-09 22:03:32 +0900

    feat(WorkerDto): 작업자 도메인 dto 구현

---
+++ 6-period/java-oncall-6-main/src/main/java/oncall/dto/WorkerDto.java
@@ -0,0 +1,22 @@
+package oncall.dto;
+
+import oncall.domain.Worker;
+
+public record WorkerDto(
+        String month,
+        String dayOfMonth,
+        String dayOfWeek,
+        String name,
+        boolean isHoliday
+) {
+
+    public static WorkerDto of(final Worker worker) {
+        return new WorkerDto(
+                String.valueOf(worker.getMonth().getNumber()),
+                String.valueOf(worker.getDayOfMonth()),
+                worker.getDayOfWeek().name(),
+                worker.getName(),
+                worker.isHoliday()
+        );
+    }
+}
