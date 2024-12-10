f9a5c0f6b38fc9dc2e46943fbb4420a1167bccfb
Mint <audwl03071@gmail.com>
2024-12-10 23:26:00 +0900

    fix: 영수증 금액 버그 수정

--- 7-period/java-convenience-store-7-main/src/main/java/store/dto/ReceiptProductDto.java
+++ 7-period/java-convenience-store-7-main/src/main/java/store/dto/ReceiptProductDto.java
@@ -6,18 +6,31 @@ public record ReceiptProductDto(List<TotalProductDto> total, List<GiftProductDto
     // 상품명		수량	금액
 
     public static ReceiptProductDto of(List<ResultDto> dtos) {
-        List<TotalProductDto> totalProductDtos = dtos.stream()
-                .map(dto -> new TotalProductDto(dto.productName(), getTotalQuantity(dto), dto.price()))
-                .toList();
-        List<GiftProductDto> giftProductDtos = dtos.stream()
+        List<TotalProductDto> totalProductDtos = makeTotalProductDtos(dtos);
+        List<GiftProductDto> giftProductDtos = makeGiftProductDtos(dtos);
+        return new ReceiptProductDto(totalProductDtos, giftProductDtos);
+    }
+
+    private static List<GiftProductDto> makeGiftProductDtos(final List<ResultDto> dtos) {
+        return dtos.stream()
                 .filter(dto -> dto.giftQuantity() > 0)
                 .map(dto -> new GiftProductDto(dto.productName(), dto.giftQuantity()))
                 .toList();
-        return new ReceiptProductDto(totalProductDtos, giftProductDtos);
+    }
+
+    private static List<TotalProductDto> makeTotalProductDtos(final List<ResultDto> dtos) {
+        return dtos.stream()
+                .map(dto -> new TotalProductDto(dto.productName(), getTotalQuantity(dto),
+                        getTotalPrice(dto.price(), getTotalQuantity(dto))))
+                .toList();
     }
 
     private static int getTotalQuantity(ResultDto dto) {
         return dto.promotionPurchaseQuantity() + dto.regularPurchaseQuantity();
+    }
+
+    public static int getTotalPrice(int price, int quantity) {
+        return price * quantity;
     }
 
     public record TotalProductDto(String name, int quantity, int price) {
