30909325cdd6782015269e4871a9b3b19590b9f5
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-10 15:42:49 +0900

    feat(StringParser): 문자열 파싱기 구현

---
+++ 7-period/java-convenience-store-7-main/src/main/java/store/util/StringParser.java
@@ -0,0 +1,33 @@
+package store.util;
+
+import java.util.Arrays;
+import java.util.List;
+import store.error.AppException;
+import store.error.ErrorMessage;
+
+public class StringParser {
+
+    private static final int SPLIT_LIMIT = -1;
+    private static final String BLANK = "";
+
+        private StringParser() {
+
+    }
+
+    public static String removePattern(final String value, final String regex) {
+        return value.replaceAll(regex, BLANK);
+    }
+
+    public static int parseToInt(final String value, final ErrorMessage errorMessage) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            throw new AppException(errorMessage);
+        }
+    }
+
+    public static List<String> parseToTokens(final String value, final String delimiter) {
+        return Arrays.stream(value.split(delimiter, SPLIT_LIMIT))
+                .toList();
+    }
+}
