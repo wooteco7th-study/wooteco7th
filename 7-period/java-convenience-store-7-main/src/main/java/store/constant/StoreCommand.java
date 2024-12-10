12c443e58c921cc453ee1b398dd39af11172eb98
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-10 22:13:00 +0900

    feat(StoreCommand): 스토어 커멘드 구현

--- 7-period/java-convenience-store-7-main/src/main/java/store/constant/StoreCommand.java
+++ 7-period/java-convenience-store-7-main/src/main/java/store/constant/StoreCommand.java
@@ -1,23 +1,23 @@
-package store.promotion.domain;
+package store.constant;
 
 import java.util.Arrays;
 import java.util.Objects;
 import store.error.AppException;
 import store.error.ErrorMessage;
 
-public enum PromotionCommand {
+public enum StoreCommand {
     YES("Y"),
     NO("N");
 
     private final String command;
 
-    PromotionCommand(final String command) {
+    StoreCommand(final String command) {
         this.command = command;
     }
 
-    public static PromotionCommand findByCommand(final String command) {
-        return Arrays.stream(PromotionCommand.values())
-                .filter(promotionCommand -> Objects.equals(promotionCommand.command, command))
+    public static StoreCommand findByCommand(final String command) {
+        return Arrays.stream(StoreCommand.values())
+                .filter(storeCommand -> Objects.equals(storeCommand.command, command))
                 .findAny()
                 .orElseThrow(() -> new AppException(ErrorMessage.INVALID_INPUT));
     }
