ae78e95766cd9f75f95562fd830fa87bf0dc172d
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-11 21:09:14 +0900

    feat(Lotto): 로또 도메인 구현

--- 7-period/java-lotto-7-main/src/main/java/lotto/domain/Lotto.java
+++ 7-period/java-lotto-7-main/src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,38 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
+import lotto.error.ErrorMessage;
+import lotto.util.ListValidator;
+import lotto.util.NumberValidator;
 
 public class Lotto {
+
+    public static final int NUMBER_MIN = 1;
+    public static final int NUMBER_MAX = 45;
+    private static final int NUMBERS_SIZE = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = numbers.stream()
+                .sorted()
+                .toList();
+    }
+
+    public boolean hasNumber(final int number) {
+        return numbers.contains(number);
+    }
+
+    public List<Integer> getNumbers() {
+        return Collections.unmodifiableList(numbers);
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
+        ListValidator.validateDuplicate(numbers, ErrorMessage.DUPLICATED_NUMBER);
+        ListValidator.validateSize(numbers, NUMBERS_SIZE, ErrorMessage.EXCEEDS_NUMBERS_SIZE);
+        ListValidator.validateRange(numbers,
+                number -> NumberValidator.validateRange(number, NUMBER_MIN, NUMBER_MAX, ErrorMessage.EXCEEDS_NUMBER));
     }
-
-    // TODO: 추가 기능 구현
 }
