f2a59ea9fe1237713cc67a298bbd009a4ebc6c15
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-11 21:31:46 +0900

    feat(LottoGroup, LottoReceiptGroup): 로또 그룹 및 dto 구현

---
+++ 7-period/java-lotto-7-main/src/main/java/lotto/domain/LottoGroup.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class LottoGroup {
+
+    private final List<Lotto> lottos;
+
+    private LottoGroup(final List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public static LottoGroup of(final Money money) {
+        final int count = money.calculateQuotient(Money.UNIT_AND_TICKET_PRICE);
+        return new LottoGroup(IntStream.range(0, count)
+                .mapToObj(idx -> QuickLottoGenerator.generate(Lotto.NUMBER_MIN, Lotto.NUMBER_MAX, Lotto.NUMBERS_SIZE))
+                .toList());
+    }
+
+    public List<Lotto> getLottos() {
+        return Collections.unmodifiableList(lottos);
+    }
+}
---
+++ 7-period/java-lotto-7-main/src/main/java/lotto/dto/LottoReceiptGroup.java
@@ -0,0 +1,23 @@
+package lotto.dto;
+
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoGroup;
+
+public record LottoReceiptGroup(
+        List<LottoReceipt> lottoReceipts
+) {
+    public static LottoReceiptGroup of(final LottoGroup lottoGroup) {
+        return new LottoReceiptGroup(lottoGroup.getLottos().stream()
+                .map(LottoReceipt::of)
+                .toList());
+    }
+
+    record LottoReceipt(
+            List<Integer> numbers
+    ) {
+        public static LottoReceipt of(final Lotto lotto) {
+            return new LottoReceipt(lotto.getNumbers());
+        }
+    }
+}
